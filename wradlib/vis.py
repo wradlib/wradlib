079d642a075f23de0daf9b933d3ddf20ee1d1eef
heistermann@ILLER_NEU.ad.geoecology.uni-potsdam.de <heistermann@ILLER_NEU.ad.geoecology.uni-potsdam.de>
2011-10-26 16:31:34 +0200

    added a polar plot to vis.py

--- wradlib/vis.py
+++ wradlib/vis.py
@@ -13,6 +13,21 @@
 """
 """
 
+def polar_plot(data):
+    n_r, n_theta = data.shape
+    R = 1.
+    theta = np.linspace(0, 2*np.pi, n_theta+1)
+    r = np.linspace(0., R, n_r + 1)
+
+    # plot as pcolormesh
+    pl.figure(figsize=(8,8))
+    pl.subplot(1,1,1, projection="polar", aspect=1.)
+    pl.jet()
+    pl.pcolormesh(theta+np.pi/2, r, np.fliplr((data)),rasterized=True)
+    pl.colorbar(shrink=0.75)
+    pl.show()
+    pl.close()
+
 
 if __name__ == '__main__':
     print 'wradlib: Calling module <vis> as main...'
