018d4537b4fd60a4837e85b75a3d468f61e0453c
heistermann@ILLER_NEU.ad.geoecology.uni-potsdam.de <heistermann@ILLER_NEU.ad.geoecology.uni-potsdam.de>
2011-10-26 14:13:50 +0200

    Adding empty files for envisaged modules.

---
+++ wradlib/atten.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:         atten
+# Purpose:
+#
+# Authors:      Maik Heistermann, Stephan Jacobi and Thomas Pfaff
+#
+# Created:      26.10.2011
+# Copyright:    (c) Maik Heistermann, Stephan Jacobi and Thomas Pfaff 2011
+# Licence:      The MIT License
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+"""
+"""
+
+
+if __name__ == '__main__':
+    print 'wradlib: Calling module <atten> as main...'
