527df5b8618e69611014bfa8bbb36a9c6ff6cdb5
heistermann@ILLER_NEU.ad.geoecology.uni-potsdam.de <heistermann@ILLER_NEU.ad.geoecology.uni-potsdam.de>
2011-10-26 11:54:58 +0200

    Test commit

---
+++ radproc.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      heistermann
+#
+# Created:     26.10.2011
+# Copyright:   (c) heistermann 2011
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
