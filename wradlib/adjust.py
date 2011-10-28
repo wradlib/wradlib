62f35916337e52a6dcf4343458d57e6161b619a0
heisterm <heisterm@uni-potsdam.de>
2011-10-28 10:04:57 +0200

    adding information to the module adjust.py docstring

--- wradlib/adjust.py
+++ wradlib/adjust.py
@@ -14,8 +14,39 @@
 Adjustment
 ^^^^^^^^^^
 
+Adjusting remotely sensed spatial data by ground truth (gage observations)
+
+The main objective of this module is the adjustment of radar-based QPE
+by rain gage observations. However, this module can also be applied to adjust
+satellite rainfall by rain gage observations, remotely sensed soil moisture
+patterns by ground truthing moisture sensors or any spatial point pattern
+which ought to be adjusted by selcted point measurements.
+
+Basically, we only need two data sources:
+
+- point observations (e.g. rain gage observations)
+
+- set of (potentially irregular) unadjusted point values (e.g. remotely sensed rainfall)
+
+.. autosummary::
+   :nosignatures:
+   :toctree: generated/
+
+
 """
 
+# site packages
+import numpy as np
+
+# wradlib modules
+import wradlib.ipol as ipol
+
+class AdjustBase():
+    """
+    The basic adjustment class
+    """
+    def __init__(self):
+        pass
 
 if __name__ == '__main__':
     print 'wradlib: Calling module <adjust> as main...'
