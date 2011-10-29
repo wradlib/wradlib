9954743df79a5a78f841b247f43002396ae8de3b
Thomas Pfaff <thomas.pfaff@iws.uni-stuttgart.de>
2011-10-29 12:48:01 +0200

    added writePolygon2Text - part of the work to transfer the functionality of nautic.py to wradlib.

--- wradlib/io.py
+++ wradlib/io.py
@@ -19,8 +19,10 @@ Raw Data I/O
    :toctree: generated/
 
    readDX
+   writePolygon2Text
 
 """
+
 
 def unpackDX(raw):
     """function removes DWD-DX-product bit-13 zero packing"""
@@ -184,5 +186,75 @@ def purgeDX(data, options):
 
     return result
 
+
+def _write_polygon2txt(f, idx, vertices):
+    f.write('%i %i\n'%idx)
+    for i, vert in enumerate(vertices):
+        f.write('%i '%(i,))
+        f.write('%f %f %f %f\n' % tuple(vert))
+
+
+def writePolygon2Text(fname, polygons):
+    """Writes Polygons to a Text file which can be interpreted by ESRI \
+    ArcGIS's "Create Features from Text File (Samples)" tool.
+
+    This is (yet) only a convenience function with limited functionality.
+    E.g. interior rings are not yet supported.
+
+    Parameters
+    ----------
+    fname : string
+        name of the file to save the vertex data to
+    polygons : list of lists
+        list of polygon vertices.
+        Each vertex itself is a list of 3 coordinate values and an
+        additional value. The third coordinate and the fourth value may be nan.
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    As Polygons are closed shapes, the first and the last vertex of each
+    polygon **must** be the same!
+
+    Examples
+    --------
+    Writes two triangle Polygons to a text file
+
+    >>> poly1 = [[0.,0.,0.,0.],[0.,1.,0.,1.],[1.,1.,0.,2.],[0.,0.,0.,0.]]
+    >>> poly2 = [[0.,0.,0.,0.],[0.,1.,0.,1.],[1.,1.,0.,2.],[0.,0.,0.,0.]]
+    >>> polygons = [poly1, poly2]
+    >>> writePolygon2Text('polygons.txt', polygons)
+
+    The resulting text file will look like this::
+
+        Polygon
+        0 0
+        0 0.000000 0.000000 0.000000 0.000000
+        1 0.000000 1.000000 0.000000 1.000000
+        2 1.000000 1.000000 0.000000 2.000000
+        3 0.000000 0.000000 0.000000 0.000000
+        1 0
+        0 0.000000 0.000000 0.000000 0.000000
+        1 0.000000 1.000000 0.000000 1.000000
+        2 1.000000 1.000000 0.000000 2.000000
+        3 0.000000 0.000000 0.000000 0.000000
+        END
+
+    """
+    with open(fname, 'w') as f:
+        f.write('Polygon\n')
+        count = 0
+        for vertices in polygons:
+            _write_polygon2txt(f, (count, 0), vertices)
+            count += 1
+        f.write('END\n')
+
+
+
 if __name__ == '__main__':
     print 'wradlib: Calling module <io> as main...'
+    import doctest
+    doctest.testmod()
