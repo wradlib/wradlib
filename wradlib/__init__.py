48b774fb7c7026a60e46637b60946b8a437bfa8c
heisterm <heisterm@uni-potsdam.de>
2011-11-10 14:37:05 +0100

    add import statements for modules in wradlib's __init__.py

--- wradlib/__init__.py
+++ wradlib/__init__.py
@@ -1,3 +1,35 @@
-"""The wradlib Package
 """
+wradlib
+=======
+
+"""
+
+import adjust
+from adjust import *
+import atten
+from atten import *
+import clutter
+from clutter import *
+import comp
+from comp import *
+import fill
+from fill import *
+import georef
+from georef import *
+import io
+from io import *
+import ipol
+from ipol import *
+import qual
+from qual import *
+import trafo
+from trafo import *
+import verify
+from verify import *
+import vis
+from vis import *
+import vpr
+from vpr import *
+import zr
+from zr import *
 
