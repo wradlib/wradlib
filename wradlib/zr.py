d42fd7a8b5152424689cd810f00ccf36a5f362b1
Thomas Pfaff <thomas.pfaff@iws.uni-stuttgart.de>
2011-11-20 21:26:26 +0100

    Added a few (poorly documented) tries at improving the performance of z2rEnhanced. Testing, which one is the fastest has yet to be done.

--- wradlib/zr.py
+++ wradlib/zr.py
@@ -28,6 +28,10 @@ into rainfall rates and vice versa
 
 
 """
+import numpy as np
+import scipy.ndimage.filters as filters
+from .trafo import decibel
+
 
 def z2r(z, a=200., b=1.6):
     """Conversion from reflectivities to rain rates.
@@ -169,6 +173,132 @@ def _z2rEnhanced(z):
     # return the results
     return r, si
 
+
+def _z2rEnhanced_md(z):
+    """multidimensional version
+    assuming the two last dimensions represent an image
+    This version should also be a bit more performant than the original because
+    it uses less for-loops"""
+    # get the shape of the input
+    dimy = z.shape[-2]
+    dimx = z.shape[-1]
+
+    # calculate the decibel values from the input
+    db = decibel(z)
+
+    # set up our output arrays
+    r = np.zeros(z.shape)
+    si = np.zeros(z.shape)
+
+    # calculate difference fields in x and y direction
+    #  mainly for performance reasons, so that we can use numpy's efficient
+    #  array operations and avoid calculating a difference more than once
+    diffx = np.abs(db[...,:,:-1] - db[...,:,1:])
+    diffy = np.abs(db[...,:-1,:] - db[...,1:,:])
+
+    # if the reflectivity is larger than 44dBZ, then there is no need to
+    # calculate the shower index
+    gt44 = db > 44.
+    r[gt44] = z2r(z[gt44], a=77, b=1.9)
+    si[gt44] = -1
+    # the same is true for values between 36.5 and 44 dBZ
+    bt3644 = (db >= 36.5) & (db<= 44.)
+    r[bt3644] = z2r(z[bt3644], a=200, b=1.6)
+    si[bt3644] = -1
+
+    # now iterate only over remaining pixels
+    wlt36 = np.where(db < 36.5)
+    nlt36 = len(wlt36[0])
+    for indices in zip(*wlt36):
+        # x and y-indices
+        i = indices[-2]
+        j = indices[-1]
+        # all remaining dimensional indices
+        ridx = indices[:-2]
+        # calculate the bounds of the region where we have to consider
+        # the respective difference
+        xmin = max(0,j-1)
+        xmax = min(dimx, j+1)
+        ymin = max(0,i-1)
+        ymax = min(dimy, i+1)
+        # in fact python is quite forgiving with upper indices
+        # ours might go one index too far, so don't try to port this
+        # to another programming language straigt away!
+        diffxcut = diffx[ridx, ymin:ymax+1, xmin:xmax]
+        diffycut = diffy[ridx, ymin:ymax, xmin:xmax+1]
+        # calculate the mean for the current pixel
+        mn = (np.sum(diffxcut) + np.sum(diffycut))  \
+              / (diffxcut.size + diffycut.size)
+        # apply the three different Z/R relations
+        if mn < 3.5:
+            r[ridx,i,j] = z2r(z[ridx,i,j], a=125, b=1.4)
+        elif mn <= 7.5:
+            r[ridx,i,j] = z2r(z[ridx,i,j], a=200, b=1.6)
+        else:
+            r[ridx,i,j] = z2r(z[ridx,i,j], a=320, b=1.4)
+        # save the shower index
+        si[ridx,i,j] = mn
+    # return the results
+    return r, si
+
+
+def z2rEsifilter(data):
+    """calculates the shower index for the enhanced z-r relation
+    to be used as the callable for a scipy.ndimate.filters.generic_filter
+    """
+    if (data[4] < 36.5):
+        tdata = data.reshape((3,3))
+        # calculate difference fields in x and y direction
+        #  mainly for performance reasons, so that we can use numpy's efficient
+        #  array operations and avoid calculating a difference more than once
+        diffx = np.abs(tdata[:,:-1] - tdata[:,1:])
+        diffy = np.abs(tdata[:-1,:] - tdata[1:,:])
+        return np.concatenate([diffx.ravel(),diffy.ravel()]).mean()
+    else:
+        return -1.
+
+
+def _z2rEnhanced_mdfilt(z):
+    """multidimensional version
+    assuming the two last dimensions represent a 2-D image
+    Uses scipy.ndimage.filters.generic_filter to reduce the number of for-loops
+    even more.
+    """
+    # get the shape of the input
+    dimy = z.shape[-2]
+    dimx = z.shape[-1]
+
+    # calculate the decibel values from the input
+    db = decibel(z)
+
+    # set up our output arrays
+    r = np.zeros(z.shape)
+    size = list(z.shape)
+    size[-2:]=[3,3]
+    size[:-2]=[1]*len(size[:-2])
+    size = tuple(size)
+    si = filters.generic_filter(db, z2rEsifilter, size=size)
+
+    gt44 = db > 44.
+    r[gt44] = z2r(z[gt44], a=77, b=1.9)
+    si[gt44] = -1.
+    # the same is true for values between 36.5 and 44 dBZ
+    bt3644 = (db >= 36.5) & (db<= 44.)
+    r[bt3644] = z2r(z[bt3644], a=200, b=1.6)
+    si[bt3644] = -2.
+
+    si1 = (si >= 0.)
+    si2 = si1 & (si < 3.5)
+    si3 = si1 &  ~si2 & (si <= 7.5)
+    si4 = si > 7.5
+
+    r[si2] = z2r(z[si2], a=125, b=1.4)
+    r[si3] = z2r(z[si3], a=200, b=1.6)
+    r[si4] = z2r(z[si4], a=320, b=1.4)
+
+    return r, si
+
+
 def z2rEnhanced(z):
     """Calculates rainrates from radar reflectivities using the enhanced
     three-part Z-R-relationship used by the DWD (as of 2009)
@@ -181,6 +311,7 @@ def z2rEnhanced(z):
     ----------
     z : a float or an array of floats
         Corresponds to reflectivity Z in mm**6/m**3
+        **must** be a 2-D array
 
     Returns
     -------
