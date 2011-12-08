19da79b5fd3ecdffc70db321e1b8c90eaa2864aa
heisterm <heisterm@uni-potsdam.de>
2011-12-08 16:40:39 +0100

    adding a basemap plot of polar radar data to vis and vis_example - still needs refinement. This is just the outline. This plot type allows polar data to be plotted on arbitrary map projections including shapefile overlay.

--- examples/vis_example.py
+++ examples/vis_example.py
@@ -11,11 +11,28 @@
 #!/usr/bin/env python
 
 import wradlib.vis as vis
+import wradlib.georef as georef
 
 
 if __name__ == '__main__':
 
     import numpy as np
-    testdata = np.loadtxt('data/polar_dBZ.dat')
-    vis.polar_plot(testdata, title='Reflectivity (dBZ)')
+
+    # EXAMPLE 1: Simple polar plot of e.g. reflectivity
+##    testdata = np.loadtxt('data/polar_dBZ_tur.gz')
+##    vis.polar_plot(testdata, title='Reflectivity (dBZ)')
+
+    # EXAMPLE 2: Plot polar data on a map
+    testdata = np.loadtxt('data/polar_dBZ_tur.gz')
+    r = np.arange(1,129)
+    az = np.linspace(0,360,361)[0:-1]
+    #   drs:  51.12527778 ; fbg: 47.87444444 ; tur: 48.58611111 ; muc: 48.3372222
+    #   drs:  13.76972222 ; fbg: 8.005 ; tur: 9.783888889 ; muc: 11.61277778
+    sitecoords = (48.5861, 9.7839)
+    polygons = georef.polar2polyvert(r, az, sitecoords)
+    baseplot = vis.PolarBasemap(polygons, sitecoords, r, az)
+    baseplot(testdata)
+
+
+
 
