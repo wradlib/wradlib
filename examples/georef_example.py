bd78ed636cfc6b9a964281a4cdb5493cfc0affde
heisterm <heisterm@uni-potsdam.de>
2011-12-08 15:24:24 +0100

    lots of changes in georef and the corresponding example: introduced a new approach for computing the polygon vertices and a more explicit method for computing the centroids of the bins. But both are founded on polar2latlon

--- examples/georef_example.py
+++ examples/georef_example.py
@@ -18,36 +18,48 @@ import wradlib.georef as georef
 if __name__ == '__main__':
     import numpy as np
     import pylab as pl
+    from matplotlib import mpl
 
     # --------------------------------------------------------------------------
     # EXAMPLE 1: Full workflow for georeferencing radar data
 
     # 1st step: generate the centroid coordinates of the radar bins
-    r  = np.array([0.,   0., 111., 111., 111., 111.,])
-    az = np.array([0., 180.,   0.,  90., 180., 270.,])
-    csite = (48.0, 9.0)
-    lat1, lon1= georef.pol2latlon(r, az, csite)
+    #   define the polar coordinates and the site coordinates in lat/lon
+    r = np.arange(1,100)
+    az = np.linspace(0,360,361)[0:-1]
+    #   drs:  51.12527778 ; fbg: 47.87444444 ; tur: 48.58611111 ; muc: 48.3372222
+    #   drs:  13.76972222 ; fbg: 8.005 ; tur: 9.783888889 ; muc: 11.61277778
+    sitecoords = (48.5861, 9.7839)
 
-    # 2nd step: project the centroid coordinates to Gaus-Krueger Zone 3
+    #   these are the polgon vertices of the radar bins
+    polygons = georef.polar2polyvert(r, az, sitecoords)
+
+    #   these are the corresponding centroids
+    cent_lon, cent_lat = georef.polar2centroids(r, az, sitecoords)
+
+    # plot the vertices and the centroids in one plot
+    fig = pl.figure(figsize=(8,8))
+    ax = fig.add_subplot(111)
+    polycoll = mpl.collections.PolyCollection(polygons,closed=True, facecolors='None')
+    ax.add_collection(polycoll, autolim=True)
+    ax.plot(cent_lon, cent_lat, 'r+')
+    ax.axis('tight')
+    pl.show()
+
+
+    # 2nd step: project the centroid coordinates to Gauss-Krueger Zone 3
+    #   this is the proj.4 projection string
     gk3 = '''
     +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel
     +towgs84=598.1,73.7,418.2,0.202,0.045,-2.455,6.7 +units=m +no_defs
     '''
-    x, y = georef.project(latc, lonc, projstr)
+    # use it for projecting the centroids to Gauss-Krueger 3
+    x, y = georef.project(cent_lat, cent_lon, gk3)
 
-    # 3rd step: generate polygon vertices
-    coords = np.arange(4).reshape((2,2))
-    coords = np.transpose(np.vstack((np.arange(5), np.repeat(1,5))))
-    coords = np.transpose(np.vstack((np.arange(5), np.arange(5))))
-    delta = 0.5
-    vertices = georef.centroid2polyvert(coords, delta)
+    # export the projected centroid coordinates
+    f = open('centroids.txt', 'w')
+    f.write('x\ty\n')
+    np.savetxt(f, np.hstack( (x.reshape((-1,1)),y.reshape((-1,1))) ), fmt='%.2f', delimiter='\t')
+    f.close()
 
-    fig = pl.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(coords[:,0], coords[:,1], 'o')
-    for vertice in vertices:
-        ax.plot(vertice[:,0], vertice[:,1], 'r+')
-    ax.set_xlim(left=-1, right=8)
-    ax.set_ylim(bottom=-1, top=8)
-    pl.show()
-
+    print 'Exit.'
