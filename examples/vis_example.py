5110021d7c3494350c8efb13b9430bdb2ad0f461
heisterm <heisterm@uni-potsdam.de>
2011-12-13 16:20:05 +0100

    still experimental map plotting in vis.py and vis_example.py, please do not use, yet

--- examples/vis_example.py
+++ examples/vis_example.py
@@ -22,17 +22,34 @@ if __name__ == '__main__':
 ##    testdata = np.loadtxt('data/polar_dBZ_tur.gz')
 ##    vis.polar_plot(testdata, title='Reflectivity (dBZ)')
 
-    # EXAMPLE 2: Plot polar data on a map
-    testdata = np.loadtxt('data/polar_dBZ_tur.gz')
-    r = np.arange(1,129)
-    az = np.linspace(0,360,361)[0:-1]
+    # EXAMPLE 2: Plot polar data on a map for Tuerkheim
+##    testdata = np.loadtxt('data/polar_dBZ_tur.gz')
+##    r = np.arange(1,129)
+##    az = np.linspace(0,360,361)[0:-1]
+##    #   drs:  51.12527778 ; fbg: 47.87444444 ; tur: 48.58611111 ; muc: 48.3372222
+##    #   drs:  13.76972222 ; fbg: 8.005 ; tur: 9.783888889 ; muc: 11.61277778
+##    sitecoords = (48.5861, 9.7839)
+##    polygons = georef.polar2polyvert(r, az, sitecoords)
+##    baseplot = vis.PolarBasemap(polygons, sitecoords, r, az)
+##    baseplot(testdata)
+
+    # EXAMPLE 3: Trying the same for the Philippines
+    import netCDF4 as nc
+    #   specify file path
+    fname='E:/data/philippines/radar/Netcdf 0926_2011_00H/SUB-20110926-000549-01-Z.nc'
+    #   read the data from file
+    testdata = nc.Dataset(fname)
+    r = np.linspace(0.5, testdata.getncattr('MaximumRange-value'), len(testdata.dimensions['Gate']))
+    az = np.round(np.array(testdata.variables['Azimuth']),0)
     #   drs:  51.12527778 ; fbg: 47.87444444 ; tur: 48.58611111 ; muc: 48.3372222
     #   drs:  13.76972222 ; fbg: 8.005 ; tur: 9.783888889 ; muc: 11.61277778
-    sitecoords = (48.5861, 9.7839)
+    sitecoords = (testdata.Latitude, testdata.Longitude)
     polygons = georef.polar2polyvert(r, az, sitecoords)
     baseplot = vis.PolarBasemap(polygons, sitecoords, r, az)
-    baseplot(testdata)
-
+##    data = np.array(testdata.variables[testdata.TypeName])
+##    data = np.where(data==testdata.MissingData, 0., data)
+    data = np.ma.masked_values(testdata.variables[testdata.TypeName], testdata.MissingData)
+    baseplot(data)
 
 
 
