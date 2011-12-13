6f120e1740b14f05758854a02f4891f7806d5564
heisterm <heisterm@uni-potsdam.de>
2011-12-13 16:19:19 +0100

    added a class to verify.py which extracts radar bins in the neighbourhood of a rain gage, added a corresponding example workflow in verify_example.py

---
+++ examples/verify_example.py
@@ -0,0 +1,59 @@
+# -*- coding: UTF-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        georef_example
+# Purpose:
+#
+# Author:      Maik Heistermann
+#
+# Created:     28.10.2011
+# Copyright:   (c) Maik Heistermann 2011
+# Licence:     The MIT License
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+import wradlib.verify as verify
+
+if __name__ == '__main__':
+    import numpy as np
+    import pylab as pl
+    from matplotlib import mpl
+
+    # ------------------------------------------------------------------------------
+    # EXAMPLE 1: Extract bin values of a polar radar data set at rain gage locations
+
+    # define the polar coordinates and the site coordinates in lat/lon
+    r = np.arange(1,129)
+    az = np.linspace(0,360,361)[0:-1]
+    sitecoords = (48.5861, 9.7839)
+    # import the polar example radar dataset
+    testdata = np.loadtxt('data/polar_R_tur.gz')
+    # the rain gages are in Gauss-Krueger Zone 3 coordinates, so we need the
+    #   corresponding proj.4 projection string
+    projstr = '''
+    +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel
+    +towgs84=598.1,73.7,418.2,0.202,0.045,-2.455,6.7 +units=m +no_defs
+    '''
+    # these are the coordinates of the rain gages in Gauss-Krueger 3 coordinates
+    x, y = np.array([3522175, 3453575, 3456725, 3498590]), np.array([5410600, 5433600, 5437860, 5359710])
+    # <nnear> nearest radar bin shall be extracted
+    nnear = 9
+    # create an instance of PolarNeighbours
+    polarneighbs = verify.PolarNeighbours(r, az, sitecoords, projstr, x, y, nnear)
+    radar_at_gages = polarneighbs.extract(testdata)
+    print radar_at_gages
+
+    binx, biny = polarneighbs.get_bincoords()
+    binx_nn, biny_nn = polarneighbs.get_bincoords_at_points()
+
+    # plot the centroids of all radar bins (red plus signs), the rain gages (blue circles)
+    #   and the nnear neighbouring radar bins (blue plus signs)
+    fig = pl.figure(figsize=(12,12))
+    ax = fig.add_subplot(111)
+    ax.plot(binx, biny, 'r+')
+    ax.plot(binx_nn, biny_nn, 'b+')
+    ax.plot(x, y, 'bo')
+    ax.axis('tight')
+    pl.title('USE THE ZOOM TOOL TO INSPECT THE NEIGHBOURHOOD OF THE GAGES!')
+    pl.show()
+
+    print 'Exit.'
--- wradlib/verify.py
+++ wradlib/verify.py
@@ -14,7 +14,81 @@
 Verification
 ^^^^^^^^^^^^
 
+Verification mainly refers to the comparison of radar-based precipitation
+estimates to ground truth.
+
+.. autosummary::
+   :nosignatures:
+   :toctree: generated/
+
+   PolarNeighbours
+
 """
+
+import numpy as np
+from scipy.spatial import KDTree
+
+import georef
+
+class PolarNeighbours():
+    """
+    For a set of projected point coordinates, extract the neighbouring bin values
+    from a data set in polar coordinates
+
+    Parameters
+    ----------
+    r : array of floats
+    az : array of floats
+    sitecoords : sequence of floats
+    projstr : string
+    x : array of floats
+    y : array of floats
+    nnear : int
+
+    """
+    def __init__(self, r, az, sitecoords, projstr, x, y, nnear=9):
+        self.nnear = nnear
+        self.az = az
+        self.r = r
+        self.x = x
+        self.y = y
+        # compute the centroid coordinates in lat/lon
+        bin_lon, bin_lat = georef.polar2centroids(r, az, sitecoords)
+        # project the centroids to cartesian map coordinates
+        binx, biny = georef.project(bin_lat, bin_lon, projstr)
+        self.binx, self.biny = binx.ravel(), biny.ravel()
+        # compute the KDTree
+        tree = KDTree(zip(self.binx, self.biny))
+        # query the tree for nearest neighbours
+        self.dist, self.ix = tree.query(zip(x, y), k=nnear)
+    def extract(self, vals):
+        """
+        Extracts the values from an array of shape (azimuth angles, range gages)
+        which correspond to the indices computed during initialisation
+
+        Parameters
+        ----------
+        vals : array of shape (..., number of azimuth, number of range gates)
+
+        Returns
+        -------
+        output : array of shape (..., number of points, nnear)
+
+        """
+        assert vals.ndim >= 2, \
+           'Your <vals> array should at least contain an azimuth and a range dimension.'
+        assert tuple(vals.shape[-2:])==(len(self.az), len(self.r)), \
+           'The shape of your vals array does not correspond with the range and azimuths you provided for your polar data set'
+        shape = vals.shape
+        vals = vals.reshape(np.concatenate( (shape[:-2], np.array([len(self.az) * len(self.r)])) ) )
+        return vals[...,self.ix]
+    def get_bincoords(self):
+        return self.binx, self.biny
+    def get_bincoords_at_points(self):
+        return self.binx[self.ix], self.biny[self.ix]
+
+
+
 
 
 if __name__ == '__main__':
