5f8c10d5387ff5e0d7384309d64097f87f3fbcf1
heisterm <heisterm@uni-potsdam.de>
2012-09-19 09:07:59 +0200

    added code for rhi_plot provided by Kai Muehlbauer - but not tested, yet!

--- wradlib/vis.py
+++ wradlib/vis.py
@@ -25,16 +25,23 @@ Standard plotting and mapping procedures
 
 """
 
+# standard libraries
 import os.path as path
+
+# site packages
 import numpy as np
 import pylab as pl
 from matplotlib import mpl
 from mpl_toolkits.basemap import Basemap, cm
-
-import wradlib.georef as georef
-
 from matplotlib.projections import PolarAxes, register_projection
 from matplotlib.transforms import Affine2D, Bbox, IdentityTransform
+from mpl_toolkits.axisartist import SubplotHost, ParasiteAxesAuxTrans, GridHelperCurveLinear
+from mpl_toolkits.axisartist.grid_finder import FixedLocator
+import mpl_toolkits.axisartist.angle_helper as angle_helper
+
+# wradlib modules
+import wradlib.georef as georef
+
 
 class NorthPolarAxes(PolarAxes):
     '''
@@ -635,6 +642,354 @@ class Grid2Basemap():
         for coll in cs.collections:
             pl.gca().collections.remove(coll)
 
+
+def get_tick_vector(vrange,vres):
+    """Calculates Vector for tickmarks for function create_curvilinear_axes.
+
+        Calculates tickmarks according to value range and wanted resolution. If no resolution is given,
+        standard values [100., 50., 25., 20., 10., 5., 2.5, 2., 1., 0.5, 0.25, 0.2] are used.
+        The number of tickmarks is normally between 5 and 10.
+
+    Parameters
+    ----------
+    vrange : value range (first and last tickmark)
+    vres : array of tick resolution (empty list, single value, multiple values)
+
+    Returns
+    ----------
+
+    vector of tickmarks
+
+    """
+
+    x = vrange[1]-vrange[0]
+
+    if not vres:
+        for div in [100.,50.,20.,10.,5.,2.5,2.,1.,0.5,0.25,0.2]:
+            cnt = x/div
+            if cnt >= 5:
+                rem = np.mod(x,div)
+                break
+    else:
+        if vres.size > 1:
+            for div in vres:
+                cnt = x/div
+                if cnt >= 5:
+                    rem = np.mod(x,div)
+                    break
+        elif vres.size == 1:
+            cnt = x/vres
+            rem = np.mod(x,vres)
+
+    return np.linspace(vrange[0],vrange[1]-rem,num=cnt+1)
+
+def create_curvilinear_axes(fig, **kwargs):
+
+    """Creates Axis with Parasite Axis for curvilinear grid.
+
+    Parameters
+    ----------
+    fig : figure object where to create axes
+    **kwargs : some axis properties
+                {'R','H','r_res','h_res', 'a_res', 'xtitle', \
+                xunit, yunit, 'ytitle', 'atitle', 'title'}
+
+    """
+
+    # get and process arguments
+    x_range = kwargs.get('R')
+    xd = x_range[1]-x_range[0]
+    y_range = kwargs.get('H')
+    yd = y_range[1]-y_range[0]
+    axpos = kwargs.get('axpos')
+
+    x_res = np.array(kwargs.get('r_res', 0.))
+    y_res = np.array(kwargs.get('h_res', 0.))
+    print(x_res, y_res)
+    a_res = kwargs.get('a_res', 10.)
+    xunit = kwargs.get('xunit', '')
+    yunit = kwargs.get('yunit', '')
+    xtitle = kwargs.get('xtitle','Range') + ' ('+xunit+')'
+    ytitle = kwargs.get('ytitle','Height') + ' ('+yunit+')'
+    atitle = kwargs.get('atitle','$Angle$')# ($^{\circ}$)')
+    title = kwargs.get('title','Range Height Indicator')
+
+    # get tickmark vectors for x and y
+    rad = get_tick_vector(x_range[0:2], x_res)
+    hgt = get_tick_vector(y_range, y_res)
+
+    # construct transform
+    tr = Affine2D().scale(np.pi/180, 1.) + PolarAxes.PolarTransform()
+
+    # build up curvilinear grid
+    extreme_finder = angle_helper.ExtremeFinderCycle(20, 20,
+                                                     lon_cycle = 100,
+                                                     lat_cycle = None,
+                                                     lon_minmax = None,
+                                                     lat_minmax = (0, np.inf),
+                                                     )
+    grid_locator1 = angle_helper.LocatorD(a_res)
+    tick_formatter1 = angle_helper.FormatterDMS()
+    grid_locator2 = FixedLocator([i for i in rad])
+    grid_helper = GridHelperCurveLinear(tr,
+                                        extreme_finder=extreme_finder,
+                                        grid_locator1=grid_locator1,
+                                        grid_locator2=grid_locator2,
+                                        tick_formatter1=tick_formatter1,
+                                        tick_formatter2=None,
+                                        )
+
+    # generate Axis
+    ax1 = SubplotHost(fig, axpos , grid_helper=grid_helper)
+
+    # make ticklabels of right and top axis visible.
+    ax1.axis["right"].major_ticklabels.set_visible(True)
+    ax1.axis["top"].major_ticklabels.set_visible(True)
+    # but set tickmarklength to zero for better presentation
+    ax1.axis["right"].major_ticks.set_ticksize(0)
+    ax1.axis["top"].major_ticks.set_ticksize(0)
+
+    # make ticklabels of right and top axis unvisible,
+    # because we are drawing them
+    ax1.axis["left"].major_ticklabels.set_visible(False)
+    ax1.axis["bottom"].major_ticklabels.set_visible(False)
+    # and also set tickmarklength to zero for better presentation
+    ax1.axis["left"].major_ticks.set_ticksize(0)
+    ax1.axis["bottom"].major_ticks.set_ticksize(0)
+
+    # let right and top axis shows ticklabels for 1st coordinate (angle)
+    ax1.axis["right"].get_helper().nth_coord_ticks=0
+    ax1.axis["top"].get_helper().nth_coord_ticks=0
+
+    # draw grid, tickmarks and ticklabes for left (y) and bottom (x) axis
+    for xmaj in rad:
+        ax1.axvline(x=xmaj,color='k', ls=':')
+        if np.equal(np.mod(xmaj, 1), 0):
+            xmaj = np.int(xmaj)
+        ax1.text(xmaj,-yd/50.+y_range[0],str(xmaj), va='top', ha='center')
+        line = mpl.lines.Line2D([xmaj,xmaj],[-yd/80.+y_range[0], y_range[0]], color='k')
+        line.set_clip_on(False)
+        ax1.add_line(line)
+
+    for ymaj in hgt:
+        ax1.axhline(y=ymaj,color='k', ls=':')
+        if np.equal(np.mod(ymaj, 1), 0):
+            ymaj = np.int(ymaj)
+        ax1.text(-xd/80.+x_range[0],ymaj,str(ymaj).rjust(4), va='center', ha='right')
+        line = mpl.lines.Line2D([-xd/130.+x_range[0],x_range[0]],[ymaj, ymaj], color='k')
+        line.set_clip_on(False)
+        ax1.add_line(line)
+
+    # add axis to figure
+    fig.add_subplot(ax1)
+
+    # plot xy-axis labels and title
+    ax1.text(-xd/15.+x_range[0],yd/2.0+y_range[0],ytitle, va='center', ha='right', rotation='vertical')
+    ax1.text(xd/2.0+x_range[0],-yd/15.+y_range[0],xtitle, va='top', ha='center')
+    # there is no "convenient" position for the "angle" label, maybe we dont need it at all
+    # ax1.text(x_range[1],y_range[1] + yd/21.,atitle, va='top', ha='right')
+
+    # plot axis title
+    ax1.set_title(title)
+    (tx,ty) = ax1.title.get_position()
+    ax1.title.set_y(1.05 * ty)
+
+    # generate and add parasite axes with given transform
+    ax2 = ParasiteAxesAuxTrans(ax1, tr, "equal")
+    # note that ax2.transData == tr + ax1.transData
+    # Anthing you draw in ax2 will match the ticks and grids of ax1.
+    ax1.parasites.append(ax2)
+
+    ax1.set_xlim(x_range[0], x_range[1])
+    ax1.set_ylim(y_range[0], y_range[1])
+    ax1.grid(True)
+
+    return ax1, ax2
+
+
+def rhi_plot(data, **kwargs):
+    """Returns figure and pylab object of plotted data from a polar grid as an RHI (Range Height Indicator).
+        Plotting need to be done outside wradlib
+
+    The data must be an array of shape (number of azimuth angles, number of range bins).
+    The azimuth angle of 0 degrees corresponds to y-axis = 0 (east direction)
+    The azimuth angle of 90 degrees corresponds to y-axis = 0 (north direction)
+    The azimuth the angles are counted counter-clock-wise forward.
+
+    Additional `myargs` are extracted from `kwargs`, processed and/or passed
+    to the create_curvilinear_axes routine
+
+    Additional remaining `kwargs` will be passed to the pcolormesh routine displaying
+    the data. Be careful!
+
+    Parameters
+    ----------
+    data : 2-d array
+        polar grid data to be plotted
+        1st dimension must be azimuth angles, 2nd must be ranges!
+
+    'kwargs':
+
+        R : tuple of array of float and unit string
+            [display min range, display max range, data max range}, unit string
+            defaults to [0, data.shape range, data.shape range], empty string
+        H : array of array float and unit string
+            [display min height, display max height], unit string
+            defaults to [0,data.shape range ], empty string
+        theta_range: float array
+            theta range (min, max) used to display data
+        rad_range: float array
+            radial range (min, max) used to display data
+        r_res : float array of range (x) tick resolution (empty, single value, multiple values)
+        h_res : float array of height (y) tick resolution (empty, single value, multiple values)
+        a_res : float
+             sets # of angle gridlines and labels, defaults to 8, wich means 10 deg resolution
+
+        title : string
+            a title of the plot, defaults to 'Range Height Indicator'
+        xtitle : string
+            x-axis label
+            defaults to 'Range' or 'Range (km)' if R is given (mostly km)
+        ytitle : string
+            y-axis label
+            defaults to 'Height' or 'Height (km)' if H is given (mostly km)
+        atitle : string
+            angle-axis label, not used at the moment, due to inconvenient placing
+            defaults to '$Angle$')# ($^{\circ}$)'
+
+        saveto : string - path of the file in which the figure should be saved
+            if string is empty, no figure will be saved and the plot will be
+            sent to screen
+        fig : matplotlib axis object
+            if None, a new matplotlib figure will be created, otherwise we plot
+            on given figure
+        figsize : width , hight tuple in inches
+             defaults to (10,6)
+        axpos : an integer or a string
+            correponds to the positional argument of mpl_toolkits.axisartist.SubplotHost
+            defaults to '111'
+            TODO: if multiple plots are used, position and size of labels have to be corrected
+            in source code
+        colormap :  string
+            choose the colormap ("Paired" per default)
+        classes :   sequence of numerical values
+            class boundaries for plotting
+        unit : string
+            the unit of the data which is plotted
+        extend :    string
+            determines the behaviour of the colorbar: default value 'neither' produces
+            a standard colorbar, 'min' and 'max' produces an arrow at the minimum or
+            maximum end, respectively, and 'both' produces an arrow at both ends. If
+            you use class boundaries for plotting, you should typically use 'both'.
+
+    Returns
+    ----------
+    fig : figure object, just for testing and in the case of multiplot
+    pl : pylab object, just for testing and in the case of multiplot
+
+    """
+
+    n_theta, n_r = data.shape
+
+    # process kwargs
+    if kwargs:
+        key = kwargs.keys()
+        value = kwargs.values()
+        myargs = dict(zip(key, value))
+    else:
+        myargs = {}
+
+    myargs['R'] = R = myargs.pop('R',([0, n_r, n_r], ''))
+    myargs['R'] = R[0]
+    myargs['xunit'] = R[1]
+    R = R[0]
+
+    H = myargs.pop('H',([0, n_r], ''))
+    myargs['H'] = H[0]
+    myargs['yunit'] = H[1]
+    H = H[0]
+
+    axpos = myargs.pop('axpos', '111')
+    myargs['axpos'] = axpos
+
+    extend = myargs.pop('extend', 'neither')
+    classes = myargs.pop('classes', None)
+    figsize = myargs.pop('figsize', (10,6))
+    fig = myargs.pop('fig', None)
+    dunit = myargs.pop('dunit','')
+    saveto = myargs.pop('saveto','')
+    colormap = myargs.pop('colormap','Paired')
+
+    theta_range = myargs.pop('theta_range', [0,90])
+    rad_range = myargs.pop('rad_range', [0,R[2]])
+
+    # remove existing myargs from kwargs
+    # remaining kwargs are for pccolormesh routine
+    key = ['R','H','r_res','h_res', 'a_res', 'xtitle', \
+           'ytitle', 'atitle', 'title', 'figsize', \
+           'theta_range','rad_range', 'fig', 'dunit', \
+           'saveto','colormap', 'axpos', 'xunit', 'yunit']
+    if kwargs:
+        value = myargs.values()
+        for k in key:
+            if k in kwargs:
+                kwargs.pop(k)
+
+    # setup vectors
+    theta = np.linspace( 0, np.pi/2 , n_theta) # for RHI
+    r = np.linspace(0., R[2], n_r)
+    theta = theta * 180. / np.pi
+    data = np.transpose(data)
+
+    #calculate indices for data range to be plotted
+    ind_start = np.where(theta >= theta_range[0])
+    ind_stop = np.where(theta <= theta_range[1])
+    ind_start1 = ind_start[0][0]
+    ind_stop1 = ind_stop[0][-1]
+    ind_start = np.where(r >= rad_range[0])
+    ind_stop = np.where(r <= rad_range[1])
+    ind_start2 = ind_start[0][0]
+    ind_stop2 = ind_stop[0][-1]
+
+    # apply data ranges to arrays
+    theta = theta[ind_start1:ind_stop1]
+    r = r[ind_start2:ind_stop2]
+    data = data[ind_start2:ind_stop2,ind_start1:ind_stop1]
+
+    # create figure, grids etc
+    if fig==None:
+        # create a new figure object
+        fig = pl.figure(figsize=figsize)
+        ax, ax2 = create_curvilinear_axes(fig, **myargs)
+    else:
+        # plot on the axes object which was passed to this function
+        ax, ax2 = create_curvilinear_axes(fig, **myargs)
+
+    # create rectangular meshgrid of polar data
+    X,Y = np.meshgrid(theta,r)
+
+    # plot data to parasite axis
+    if classes==None:
+        # automatic color normalization by vmin and vmax (not recommended)
+        circle = ax2.pcolormesh(X, Y, data, rasterized=True, cmap=colormap, **kwargs)#, vmin=-32., vmax=95.5)
+    else:
+        # colors are assigned according to class boundaries and colormap argument
+        mycmap = pl.get_cmap(colormap, lut=len(classes))
+        mycmap = mpl.colors.ListedColormap(mycmap( np.arange(len(classes)-1) ))
+        norm   = mpl.colors.BoundaryNorm(classes, mycmap.N)
+        circle = ax2.pcolormesh(X, Y, data, rasterized=True, cmap=mycmap, norm=norm, **kwargs)
+
+    # plot colorbar
+    cbar = fig.colorbar(circle, extend=extend)
+    cbar.set_label('('+dunit+')')
+
+    if saveto!='':
+        # save plot to file
+        if ( path.exists(path.dirname(saveto)) ) or ( path.dirname(saveto)=='' ):
+            pl.savefig(saveto)
+            pl.close()
+
+    return fig, pl
 
 
 if __name__ == '__main__':
