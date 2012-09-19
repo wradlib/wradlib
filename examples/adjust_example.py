2ee690771be97075cd12e3be933e65fcbc4d50ad
heisterm <heisterm@uni-potsdam.de>
2012-09-19 14:07:51 +0200

    added an outline for a function to create a CAPPI in module VPR and added some explanatory text about significance of VPR

--- examples/adjust_example.py
+++ examples/adjust_example.py
@@ -17,66 +17,91 @@ if __name__ == '__main__':
     import numpy as np
     import pylab as pl
 
-    #---------------------------------------------------------------------------
-    # Creating synthetic data
+    # 1-d example
     # --------------------------------------------------------------------------
-    # number of points of raw per axis (total number of points of raw will be num_raw**2)
-    num_raw = 100
-    num_obs = 50
-    # making raw coordinates
-    raw_coords = np.meshgrid(np.linspace(0,100,num_raw), np.linspace(0,100,num_raw))
-    raw_coords = np.vstack((raw_coords[0].ravel(), raw_coords[1].ravel())).transpose()
-    # making raw data
-    raw = np.abs(np.sin(0.1*raw_coords).sum(axis=1))
-    # indices for creating obs from raw
-    obs_ix = np.random.uniform(low=0, high=num_raw**2, size=num_obs).astype('i4')
-    # creating obs_coordinates
-    obs_coords = raw_coords[obs_ix]
-    # creating obs data by perturbing raw
-    obs = raw[obs_ix]+np.random.uniform(low=-1., high=1, size=len(obs_ix))
-    obs = np.abs(obs)
-
-    #---------------------------------------------------------------------------
-    # Gage adjustment
-    #---------------------------------------------------------------------------
-    adjuster = adjust.AdjustAdd(obs_coords, raw_coords, stat='median', p_idw=2.)
-    result = adjuster(obs, raw)
-
-    #---------------------------------------------------------------------------
-    # Plotting
-    #---------------------------------------------------------------------------
-    # maximum value for normalisation
-    maxval = np.max(np.concatenate((raw, obs, result)).ravel())
-    # open figure
-    fig = pl.figure()
-    # adding subplot for unadjusted
-    ax = fig.add_subplot(221, aspect='equal')
-    raw_plot = ax.scatter(raw_coords[:,0], raw_coords[:,1], c=raw, vmin=0, vmax=maxval, edgecolor='none')
-    ax.scatter(obs_coords[:,0], obs_coords[:,1], c=obs.ravel(), marker='s', s=50, vmin=0, vmax=maxval)
-    pl.colorbar(raw_plot)
-    pl.title('Raw field and observations')
-    # adding subplot for adjusted
-    ax = fig.add_subplot(222, aspect='equal')
-    raw_plot = ax.scatter(raw_coords[:,0], raw_coords[:,1], c=result, vmin=0, vmax=maxval, edgecolor='none')
-    ax.scatter(obs_coords[:,0], obs_coords[:,1], c=obs.ravel(), marker='s', s=50, vmin=0, vmax=maxval)
-    pl.colorbar(raw_plot)
-    pl.title('Adjusted field and observations')
-    # scatter plot raw vs. obs
-    ax = fig.add_subplot(223, aspect='equal')
-    ax.scatter(obs, raw[obs_ix])
-    ax.plot([0,maxval],[0,maxval],'-', color='grey')
-    pl.title('Scatter plot raw vs. obs')
-    ax.set_xlim(left=0)
-    ax.set_ylim(bottom=0)
-    # scatter adjusted vs. raw (for control purposes)
-    ax = fig.add_subplot(224, aspect='equal')
-    ax.scatter(obs, result[obs_ix])
-    ax.plot([0,maxval],[0,maxval],'-', color='grey')
-    ax.set_xlim(left=0)
-    ax.set_ylim(bottom=0)
-    pl.title('Scatter plot adjusted vs. obs')
-
+    # gage and radar coordinates
+    obs_coords = np.array([5,10,15,20,30,45,65,70,77,90])
+    radar_coords = np.arange(0,101)
+    # true rainfall
+    truth = np.abs(np.sin(0.1*radar_coords))
+    # radar error
+    erroradd = np.random.uniform(0,0.5,len(radar_coords))
+    errormult= 1.1
+    # radar observation
+    radar = errormult*truth + erroradd
+    # gage observations are assumed to be perfect
+    obs = truth[obs_coords]
+    # adjust the radar observation by additive model
+    add_adjuster = adjust.AdjustAdd(obs_coords, radar_coords, nnear_raws=3)
+    add_adjusted = add_adjuster(obs, radar)
+    line1 = pl.plot(radar_coords, radar, 'b-', label="raw radar")
+    line2 = pl.plot(obs_coords, obs, 'ro', label="gage obs")
+    line3 = pl.plot(radar_coords, add_adjusted, 'r-', label="adjusted by AdjustAdd")
+    pl.legend()
     pl.show()
-    pl.close()
+
+
+##    # 2d example
+##    #---------------------------------------------------------------------------
+##    # Creating synthetic data
+##    # --------------------------------------------------------------------------
+##    # number of points of raw per axis (total number of points of raw will be num_raw**2)
+##    num_raw = 100
+##    num_obs = 50
+##    # making raw coordinates
+##    raw_coords = np.meshgrid(np.linspace(0,100,num_raw), np.linspace(0,100,num_raw))
+##    raw_coords = np.vstack((raw_coords[0].ravel(), raw_coords[1].ravel())).transpose()
+##    # making raw data
+##    raw = np.abs(np.sin(0.1*raw_coords).sum(axis=1))
+##    # indices for creating obs from raw
+##    obs_ix = np.random.uniform(low=0, high=num_raw**2, size=num_obs).astype('i4')
+##    # creating obs_coordinates
+##    obs_coords = raw_coords[obs_ix]
+##    # creating obs data by perturbing raw
+##    obs = raw[obs_ix]+np.random.uniform(low=-1., high=1, size=len(obs_ix))
+##    obs = np.abs(obs)
+##
+##    #---------------------------------------------------------------------------
+##    # Gage adjustment
+##    #---------------------------------------------------------------------------
+##    adjuster = adjust.AdjustAdd(obs_coords, raw_coords, stat='median', p_idw=2.)
+##    result = adjuster(obs, raw)
+##
+##    #---------------------------------------------------------------------------
+##    # Plotting
+##    #---------------------------------------------------------------------------
+##    # maximum value for normalisation
+##    maxval = np.max(np.concatenate((raw, obs, result)).ravel())
+##    # open figure
+##    fig = pl.figure()
+##    # adding subplot for unadjusted
+##    ax = fig.add_subplot(221, aspect='equal')
+##    raw_plot = ax.scatter(raw_coords[:,0], raw_coords[:,1], c=raw, vmin=0, vmax=maxval, edgecolor='none')
+##    ax.scatter(obs_coords[:,0], obs_coords[:,1], c=obs.ravel(), marker='s', s=50, vmin=0, vmax=maxval)
+##    pl.colorbar(raw_plot)
+##    pl.title('Raw field and observations')
+##    # adding subplot for adjusted
+##    ax = fig.add_subplot(222, aspect='equal')
+##    raw_plot = ax.scatter(raw_coords[:,0], raw_coords[:,1], c=result, vmin=0, vmax=maxval, edgecolor='none')
+##    ax.scatter(obs_coords[:,0], obs_coords[:,1], c=obs.ravel(), marker='s', s=50, vmin=0, vmax=maxval)
+##    pl.colorbar(raw_plot)
+##    pl.title('Adjusted field and observations')
+##    # scatter plot raw vs. obs
+##    ax = fig.add_subplot(223, aspect='equal')
+##    ax.scatter(obs, raw[obs_ix])
+##    ax.plot([0,maxval],[0,maxval],'-', color='grey')
+##    pl.title('Scatter plot raw vs. obs')
+##    ax.set_xlim(left=0)
+##    ax.set_ylim(bottom=0)
+##    # scatter adjusted vs. raw (for control purposes)
+##    ax = fig.add_subplot(224, aspect='equal')
+##    ax.scatter(obs, result[obs_ix])
+##    ax.plot([0,maxval],[0,maxval],'-', color='grey')
+##    ax.set_xlim(left=0)
+##    ax.set_ylim(bottom=0)
+##    pl.title('Scatter plot adjusted vs. obs')
+##
+##    pl.show()
+##    pl.close()
 
 
--- wradlib/vpr.py
+++ wradlib/vpr.py
@@ -11,10 +11,57 @@
 #!/usr/bin/env python
 
 """
-Vertical Profile of Reflectivity
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Vertical Profile of Reflectivity (VPR)
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+UNDER DEVELOPMENT
+
+Precipitation is 3-dimensional in space. The vertical distribution of precipitation
+(and thus reflectivity) is typically non-uniform. As the height of the radar beam increases with the distance
+from the radar location (beam elevation, earth curvature), the one sweep samples
+from different heights. The effects of the non-uniform VPR and the differnt sampling heights need
+to be accounted for if we are interested in the precipiation near the ground or
+in defined heights. This module is intended to provide a set of tools to account
+for these effects.
+
+.. autosummary::
+   :nosignatures:
+   :toctree: generated/
+
+   cappi
 
 """
+
+def cappi(data, sitecoords, elevs, levels, dx, dy, maxvert, maxhoriz, projstr):
+    """UNDER DEVELOPMENT: Create a CAPPI from sweep data with multiple elevation angles
+
+    Parameters
+    ----------
+    data : float ndarray with shape (num elevations, num azimuth angles, num range bins)
+    sitecoords : sequence of three floats indicating the radar position
+       (latitude in decimal degrees, longitude in decimal degrees, height a.s.l. in meters)
+    elevs : sequence of elevation angles corresponding to first dimension of data
+    levels : sequence of floats
+       target altitudes for CAPPI (in meters)
+    dx : float
+       horizontal resolution of CAPPI in x direction
+    dy : float
+       horizontal resolution of CAPPI in y direction
+    maxvert : float
+       maximum vertical distance threshold - the next data bin must be closer to
+       the target location than maxvert in order to assign a value
+    maxhoriz : float
+       maximum horizontal distance threshold - the next data bin must be closer to
+       the target location than maxhoriz in order to assign a value
+    projstr : proj.4 projection string
+
+    Returns
+    -------
+    output : float ndarray of shape (number of levels, number of x coordinates, number of y coordinates)
+
+    """
+    pass
+
 
 
 if __name__ == '__main__':
