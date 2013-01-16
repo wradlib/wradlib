69838119a1a8c33221e51b17278a06bc1e382774
heisterm <heisterm@uni-potsdam.de>
2013-01-16 11:00:45 +0100

    ENH: wradlib.vpr.volcoords_from_polar_irregular can deal with volumes in which the scan strategy differs between elevation angles.

--- wradlib/vpr.py
+++ wradlib/vpr.py
@@ -24,7 +24,7 @@ to provide a set of tools to account for these effects.
 
 The first step will normally be to reference the polar volume data in a 3-dimensional
 Cartesian coordinate system. The three dimensional Cartesian coordinates of the
-original polar volume data can be computed using :doc:`volcoords_from_polar`.
+original polar volume data can be computed using :doc:`volcoords_from_polar <generated/wradlib.vpr.volcoords_from_polar>`.
 
 Then, we can create regular 3-D grids in order to analyse the vertical profile
 of reflectivity or rainfall intensity. For some applications you might want
@@ -252,7 +252,7 @@ class CartesianVolume():
                     except:
                         print "Cannot save mask to file..."
                         raise Exception()
-    def _get_mask(self, cartcoords, polcoords=None, polshape=None, maxrange=None):
+    def _get_mask(self, cartcoords, polcoords=None, polshape=None, maxrange=None, maskfile=None):
         """Returns a mask (the base class only contains a dummy function which masks nothing)
 
         This method needs to be replaced for inherited classes such as CAPPI or PseudoCAPPI
@@ -426,6 +426,8 @@ def volcoords_from_polar(sitecoords, elevs, azimuths, ranges, projstr=None):
     output : array of shape (num volume bins, 3)
 
     """
+    # make sure that elevs is an array
+    elevs = np.array([elevs]).ravel()
     # create polar grid
     el, az, r = util.meshgridN(elevs, azimuths, ranges)
     # get geographical coordinates
@@ -435,6 +437,90 @@ def volcoords_from_polar(sitecoords, elevs, azimuths, ranges, projstr=None):
     # create standard shape
     coords = np.vstack((x.ravel(),y.ravel(),z.ravel())).transpose()
     return coords
+
+
+def volcoords_from_polar_irregular(sitecoords, elevs, azimuths, ranges, projstr=None):
+    """Create Cartesian coordinates for the polar volume bins
+
+    Parameters
+    ----------
+    sitecoords : sequence of three floats indicating the radar position
+       (latitude in decimal degrees, longitude in decimal degrees, height a.s.l. in meters)
+    elevs : sequence of elevation angles
+    azimuths : sequence of azimuth angles
+    ranges : sequence of ranges
+    projstr : proj.4 projection string
+
+    Returns
+    -------
+    output : array of shape (num volume bins, 3)
+
+    """
+    # check structure: Are azimuth angles and range bins the same for each elevation angle?
+    oneaz4all = True
+    onerange4all = True
+    #   check elevs array, first: must be one-dimensional
+    try:
+        elevs = np.array(elevs)
+    except:
+        print "Could not create an array from argument <elevs>."
+        print "The following exception was raised:"
+        raise
+    assert (elevs.ndim==1) and (elevs.dtype!=np.dtype("object")), "Argument <elevs> in wradlib.wolcoords_from_polar must be a 1-D array."
+    #   now: is there one azimuths array for all elevation angles or one for each?
+    try:
+        azimuths = np.array(azimuths)
+    except:
+        print "Could not create an array from argument <azimuths>."
+        print "The following exception was raised:"
+        raise
+    if len(azimuths)==len(elevs):
+        # are the items of <azimuths> arrays themselves?
+        isseq = [util.issequence( elem ) for elem in azimuths]
+        assert not ( (False in isseq) and (True in isseq) ), "Argument <azimuths> contains both iterable and non-iterable items."
+        if True in isseq:
+            # we expect one azimuth array for each elevation angle
+            oneaz4all = False
+    #   now: is there one ranges array for all elevation angles or one for each?
+    try:
+        ranges = np.array(ranges)
+    except:
+        print "Could not create an array from argument <ranges>."
+        print "The following exception was raised:"
+        raise
+    if len(ranges)==len(elevs):
+        # are the items of <azimuths> arrays themselves?
+        isseq = [util.issequence( elem ) for elem in ranges]
+        assert not ( (False in isseq) and (True in isseq) ), "Argument <azimuths> contains both iterable and non-iterable items."
+        if True in isseq:
+            # we expect one azimuth array for each elevation angle
+            onerange4all = False
+    if oneaz4all and onerange4all:
+        # this is the simple way
+        return volcoords_from_polar(sitecoords, elevs, azimuths, ranges, projstr)
+    # No simply way, so we need to construct the coordinates arrays for each elevation angle
+    #   but first adapt input arrays to this task
+    if onerange4all:
+        ranges = np.array([ranges for i in range(len(elevs))])
+    if oneaz4all:
+        azimuths = np.array([azimuths for i in range(len(elevs))])
+    #   and second create the corresponding polar volume grid
+    el=np.array([])
+    az=np.array([])
+    r =np.array([])
+    for i, elev in enumerate(elevs):
+        az_tmp, r_tmp = np.meshgrid(azimuths[i], ranges[i])
+        el = np.append(el, np.repeat(elev, len(azimuths[i])*len(ranges[i])) )
+        az = np.append(az, az_tmp.ravel())
+        r  = np.append(r,  r_tmp.ravel())
+    # get geographical coordinates
+    lats, lons, z = georef.polar2latlonalt(r, az, el, sitecoords, re=6370040.)
+    # get projected horizontal coordinates
+    x, y = georef.project(lats, lons, projstr)
+    # create standard shape
+    coords = np.vstack((x.ravel(),y.ravel(),z.ravel())).transpose()
+    return coords
+
 
 
 def synthetic_polar_volume(coords):
