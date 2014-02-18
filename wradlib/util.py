67842b3841eded4e8213b895857feed93864e4a0
heisterm <heisterm@uni-potsdam.de>
2014-02-18 16:29:31 +0100

    ENH: Added a deprecation decorator function in the util module.
    The decorator can be used in order to mark deprecated functions. If these functions
    are called, a DeprecationWarning will be raised.
    
    In order to use the decorator, just
    
    >>> from util import deprecated
    
    >>> @deprecated()
    ... def foo(x)
    ...    return(x)

--- wradlib/util.py
+++ wradlib/util.py
@@ -34,6 +34,57 @@ from scipy.spatial import cKDTree
 from scipy.stats import nanmean
 import importlib
 
+import warnings
+import functools
+warnings.simplefilter('once', DeprecationWarning)
+
+
+
+def deprecated(replacement=None):
+    """A decorator which can be used to mark functions as deprecated.
+    replacement is a callable that will be called with the same args
+    as the decorated function.
+
+    Author: Giampaolo Rodola' <g.rodola [AT] gmail [DOT] com>
+    License: MIT
+
+    >>> @deprecated()
+    ... def foo(x):
+    ...     return x
+    ...
+    >>> ret = foo(1)
+    DeprecationWarning: foo is deprecated
+    >>> ret
+    1
+    >>>
+    >>>
+    >>> def newfun(x):
+    ...     return 0
+    ...
+    >>> @deprecated(newfun)
+    ... def foo(x):
+    ...     return x
+    ...
+    >>> ret = foo(1)
+    DeprecationWarning: foo is deprecated; use newfun instead
+    >>> ret
+    0
+    >>>
+    """
+    def outer(fun):
+        msg = "psutil.%s is deprecated" % fun.__name__
+        if replacement is not None:
+            msg += "; use %s instead" % replacement
+        if fun.__doc__ is None:
+            fun.__doc__ = msg
+
+        @functools.wraps(fun)
+        def inner(*args, **kwargs):
+            warnings.warn(msg, category=DeprecationWarning, stacklevel=2)
+            return fun(*args, **kwargs)
+
+        return inner
+    return outer
 
 
 def import_optional(module):
