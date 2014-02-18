dca27eb76e044c8db2f2f061a688cc17285fe98a
heisterm <heisterm@uni-potsdam.de>
2014-02-18 12:10:37 +0100

    REF: Removed all "from module import *" statements that import funcitons directly into the wradlib namespace.
    This requries refactoring of all code that calls wradlib funcitons directly from
    the wradlib namespace instead of from the corresponding modules.
    
    So code such as
    
    >>> wradlib.readDX(f)
    
    will not work anymore. Instead, you need to specifiy the module by
    
    >>> wradlib.io.readDX(f)
    
    This change is a necessary requirement to have a better namespace hygiene.

--- wradlib/__init__.py
+++ wradlib/__init__.py
@@ -5,40 +5,34 @@ wradlib
 """
 
 import adjust
-from adjust import *
+##from adjust import *
 import bufr
-from bufr import *
+##from bufr import *
 import atten
-from atten import *
+##from atten import *
 import clutter
-from clutter import *
+##from clutter import *
 import comp
-from comp import *
+##from comp import *
 import fill
-from fill import *
+##from fill import *
 import georef
-from georef import *
+##from georef import *
 import io
-from io import *
+##from io import *
 import ipol
-from ipol import *
+##from ipol import *
 import qual
-from qual import *
+##from qual import *
 import trafo
-from trafo import *
+##from trafo import *
 import verify
-from verify import *
+##from verify import *
 import vis
-from vis import *
+##from vis import *
 import vpr
-from vpr import *
+##from vpr import *
 import zr
-from zr import *
+##from zr import *
 import util
-from util import *
-try:
-    import speedup
-    from speedup import *
-except ImportError:
-    print "WARNING: To increase performance of differential phase processing, you should try to build module <speedup>."
-    print "See module documentation for details."
+##from util import *
