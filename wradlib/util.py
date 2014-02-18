7f4d0c08e963ac39f2d42ae76f89e06f86239fb5
Thomas Pfaff <toromtomtom@googlemail.com>
2014-02-18 17:51:51 +0100

    ENH: refined optional_import to allow global imports.
    function now always returns an object (the actual module on success,
    a stub object on failure), so that imports may also be done globally
    without breaking the code on a failed import. The lazy import will be
    revealed as soon as any attribute is requested from the stub object.
    
    This way failed optional imports won't clutter your command window with
    warnings but will give you a clear error message, once you try to access
    the contents of the missing module.

--- wradlib/util.py
+++ wradlib/util.py
@@ -84,17 +84,73 @@ def deprecated(replacement=None):
         return inner
     return outer
 
+class OptionalModuleStub(object):
+    """Stub class for optional imports.
+
+    Objects of this class are instantiated when optional modules are not
+    present on the user's machine.
+    This allows global imports of optional modules with the code only breaking
+    when actual attributes from this module are called.
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def __getattr__(self, name):
+        raise AttributeError, ('Module "'+ self.name +
+                           '" is not installed.\n\n' +
+                           'You tried to access function/module/attribute "' +
+                            name + '"\nfrom module "' + self.name + '".\nThis '+
+                            'module is optional right now in wradlib.\n' +
+                            'You need to separately install this dependency.'
+                            )
+
 
 def import_optional(module):
-    """Allowing for lazy loading of optional wradlib modules or dependencies
+    """Allowing for lazy loading of optional wradlib modules or dependencies.
 
+    This function removes the need to satisfy all dependencies of wradlib before
+    being able to work with it.
+
+    Parameters:
+    -----------
+    module : string
+             name of the module
+
+    Returns:
+    --------
+    mod : object
+          if module is present, returns the module object, on ImportError
+          returns an instance of `OptionalModuleStub` which will raise an
+          AttributeError as soon as any attribute is accessed.
+
+    Examples:
+    ---------
+    Trying to import a module that exists makes the module available as normal.
+    You can even use an alias. You cannot use the '*' notation, or import only
+    select functions, but you can simulate most of the standard import syntax
+    behavior
+    >>> m = import_optional('math')
+    >>> m.log10(100)
+    2.0
+
+    Trying to import a module that does not exists, does not produce any errors.
+    Only when some function is used, the code triggers an error
+    >>> m = import_optional('nonexistentmodule')
+    >>> m.log10(100)
+    Traceback (most recent call last):
+    ...
+    AttributeError: Module "nonexistentmodule" is not installed.
+
+    You tried to access function/module/attribute "log10"
+    from module "nonexistentmodule".
+    This module is optional right now in wradlib.
+    You need to separately install this dependency.
     """
     try:
         mod = importlib.import_module(module)
     except ImportError:
-        raise ImportError, 'wradlib optional module "%s" not '%(module,)+ \
-        'found. please see http://wradlib.bitbucket.org/opt.html on hints '+\
-        'how to solve this problem.'
+        mod = OptionalModuleStub(module)
+
     return mod
 
 
