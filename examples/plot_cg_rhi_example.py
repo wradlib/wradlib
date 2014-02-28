f6841658934de542149bdc75e4f5ecc05fb78975
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2014-02-28 12:42:07 +0000

    MIN: adjustet figure #7 from plot_cg_rhi_example.py

--- examples/plot_cg_rhi_example.py
+++ examples/plot_cg_rhi_example.py
@@ -207,11 +207,11 @@ cgax, caax, paax, pm = wradlib.vis.plot_cg_rhi(ma, r, th,
                                                rf=1e3, autoext=True,
                                                refrac=True, subplot=211)
 t = pl.title('CG RHI refrac=True')
-t.set_y(1.05)
+t.set_y(1.1)
 cbar = pl.gcf().colorbar(pm)
-caax.set_xlabel('range [m]')
-caax.set_ylabel('height [m]')
-pl.text(1.0, 1.05, 'elevation', transform=caax.transAxes,
+caax.set_xlabel('range [km]')
+caax.set_ylabel('height [km]')
+pl.text(1.0, 1.15, 'elevation', transform=caax.transAxes,
         va='bottom', ha='right')
 cbar.set_label('reflectivity [dBZ]')
 # zoom 
@@ -222,19 +222,21 @@ paax.plot(15, 42, 'bo', label="paax")
 caax. plot(40,11,'ro', label="caax")
 cgax.legend()
 pl.tight_layout()
+pl.subplots_adjust(hspace=0.4)
 
-# figure #7-1
+
+# figure #7-2
 # check how refraction is doing
 # and plot some annotation and colorbar
 cgax, caax, paax, pm = wradlib.vis.plot_cg_rhi(ma, r, th,
                                                rf=1e3, autoext=True,
                                                refrac=False, subplot=212)
 t = pl.title('CG RHI refrac=False')
-t.set_y(1.05)
+t.set_y(1.1)
 cbar = pl.gcf().colorbar(pm)
 caax.set_xlabel('range [km]')
 caax.set_ylabel('height [km]')
-pl.text(1.0, 1.05, 'elevation', transform=caax.transAxes,
+pl.text(1.0, 1.15, 'elevation', transform=caax.transAxes,
         va='bottom', ha='right')
 cbar.set_label('reflectivity [dBZ]')
 # zoom 
