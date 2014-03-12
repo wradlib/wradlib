a962e8dde62cca5d45015a53f048f032ccecf77d
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2014-03-12 15:28:21 +0000

    ENH: transformed tests to use 'unittest` module

--- wradlib/tests/test_io.py
+++ wradlib/tests/test_io.py
@@ -1,34 +1,21 @@
 
-import wradlib.io as io
 
-# testing functions related to readDX
-def test___getTimestampFromFilename():
-    pass
+import unittest
 
-def test__getDXTimestamp():
-    pass
 
-def test__unpackDX():
-    pass
+class IOTest(unittest.TestCase):
+    # testing functions related to readDX
+    def test_getTimestampFromFilename(self):
+        pass
 
-def test__readDX():
-    pass
+    def test_getDXTimestamp(self):
+        pass
 
-def test__filter_gabella_a_tr1():
-    data = np.arange(9)
-    data[4] = 10
-    result = cl._filter_gabella_a(data, tr1=5)
-    assert result == 3
+    def test_unpackDX(self):
+        pass
 
-#-------------------------------------------------------------------------------
-# testing the first part of the filter
-#-------------------------------------------------------------------------------
-def filter_setup():
-    img = np.zeros((10,10), dtype=np.float32)
-    img[2,2] = 10    # isolated pixel
-    img[3,8:10] = 10 # line
-    img[5,:] = 5     # spike
-    img[7:9,7:9] = 5 # precip field
+    def test_readDX(self):
+        pass
 
-def test_filter_gabella_a():
-    pass
+if __name__ == '__main__':
+    unittest.main()
