1a82819e97cfdd990271b1701e647db25717e15d
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2014-03-12 15:28:21 +0000

    ENH: transformed tests to use 'unittest` module

--- wradlib/tests/test_trafo.py
+++ wradlib/tests/test_trafo.py
@@ -9,22 +9,32 @@
 # Licence:     The MIT License
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
+import unittest
 import numpy as np
 import wradlib.trafo as trafo
 
 
-def test__rvp2dBZ():
-    assert np.allclose(trafo.rvp2dBZ(np.array([0.,128.,255.])), np.array([-32.5,31.5,95.0]))
+class TransformationTest(unittest.TestCase):
 
+    def setUp(self):
+        self.rvp = np.array([0.,128.,255.])
+        self.dbz = np.array([-32.5,31.5,95.0])
+        self.lin = np.array([1e-4, 1, 1e4])
+        self.dec = np.array([-40,0,40])
+        self.r = np.array([5., 10., 20.])
 
-def test__decibel():
-    assert np.allclose(trafo.decibel(np.array([1e-4, 1, 1e4])), np.array([-40,0,40]))
+    def test_rvp2dBZ(self):
+        self.assertTrue(np.allclose(trafo.rvp2dBZ(self.rvp), self.dbz))
 
+    def test_decibel(self):
+        self.assertTrue(np.allclose(trafo.decibel(self.lin), self.dec))
 
-def test__idecibel():
-    assert np.allclose(trafo.idecibel(np.array([-40, 0, 40])), np.array([1e-4, 1, 1e4]))
+    def test_idecibel(self):
+        self.assertTrue(np.allclose(trafo.idecibel(self.dec), self.lin))
 
+    def test_r2depth(self):
+        self.assertTrue(np.allclose(trafo.r2depth(self.r,720), np.array([1.,2.,4.])))
+        self.assertTrue(np.allclose(trafo.r2depth(self.r,360), np.array([0.5,1.,2.])))
 
-def test__r2depth():
-    assert np.allclose(trafo.r2depth(np.array([5., 10., 20.]),720), np.array([1.,2.,4.]))
-    assert np.allclose(trafo.r2depth(np.array([5., 10., 20.]),360), np.array([0.5,1.,2.]))
+if __name__ == '__main__':
+    unittest.main()
