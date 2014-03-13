4be141781bc89de60d38b32923d81da84fa8cbcf
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2014-03-13 15:35:08 +0000

    OPT: changed setup.py to use setuptools and distutils

--- setup.py
+++ setup.py
@@ -51,8 +51,9 @@ clutter or attenuation) and visualising the data.
           'Programming Language :: Python',
           'Topic :: Scientific/Engineering',
           ],
-          install_requires=["numpydoc >= 0.3", "pyproj >= 1.8",
-                            "netCDF4 >= 1.0", "h5py >= 2.0.1",
-                            "matplotlib >= 1.1.0", "scipy >= 0.9", "numpy >= 1.7.0"]
+          install_requires=["numpy >= 1.7.0", "scipy >= 0.9",
+                            "matplotlib >= 1.1.0",
+                            "numpydoc >= 0.3", "pyproj >= 1.8",
+                            "netCDF4 >= 1.0", "h5py >= 2.0.1"]
           )
 
