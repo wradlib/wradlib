bd2c88fb816d9f41b324aa9aae66f6e39ed697b4
k.muehlbauer@KaiBox.fritz.box <k.muehlbauer@KaiBox.fritz.box>
2014-04-03 21:56:21 +0200

    MIN: First running implementation of examples tests
    --HG--
    branch : testing

--- runtests.py
+++ runtests.py
@@ -1,23 +1,47 @@
-__author__ = 'k.muehlbauer'
+#------------------------------------------------------------------------------
+# Name:        runtest.py
+# Purpose:     testrunner for unittest, doctest and examples test
+#
+# Author:      Kai Muehlbauer
+#
+# Created:     03.03.2014
+# Copyright:   (c) Kai Muehlbauer 2014
+# Licence:     The MIT License
+#------------------------------------------------------------------------------
 
 import unittest
 import doctest
 import os
 import glob
 
-
-
-# files = glob.glob(root_dir + 'test_*.py')
-# module_strings = [str[0:len(str)-3].replace('/','.') for str in files]
-# #module_strings.replace('/', '.')
-# print(module_strings)
-# suites = [unittest.defaultTestLoader.loadTestsFromName(str) for str in module_strings]
-# testSuite = unittest.TestSuite(suites)
-
 testSuite = []
 
+# examples test
+root_dir = 'examples/'
+files = []
+skip = ['__init__.py', 'bufr.py', 'test_']
+for root, _, filenames in os.walk(root_dir):
+    for filename in filenames:
+        if filename in skip or filename[-3:] != '.py':
+            continue
+        if 'examples/data' in root:
+            continue
+        f = os.path.join(root, filename)
+        f = f.replace('/', '.')
+        f = f[:-3]
+        files.append(f)
+import inspect
+suite = unittest.TestSuite()
+for module in files:
+    module1, func = module.split('.')
+    module = __import__(module)
+    func = getattr(module, func)
+    funcs = inspect.getmembers(func, inspect.isfunction)
+    [suite.addTest(unittest.FunctionTestCase(v)) for k,v in funcs if k.startswith("ex_") ]
 
-# Doctest
+testSuite.append(unittest.TestSuite(suite))
+
+# doctest
 root_dir = 'wradlib/'
 files = []
 skip = ['__init__.py', 'bufr.py', 'test_']
@@ -39,8 +63,7 @@ for module in files:
     suite.addTest(doctest.DocTestSuite(module))
 testSuite.append(unittest.TestSuite(suite))
 
-
-
+# unittest
 files = []
 skip = ['__init__.py']
 root_dir = 'wradlib/tests/'
