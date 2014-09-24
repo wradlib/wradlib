54deaeb1af03dd60fe1bd27cf2848e4275f88fc2
radproc@sv30.meteo.uni-bonn.de <radproc@sv30.meteo.uni-bonn.de>
2014-09-24 10:54:06 +0200

    ENH: introduced 'pure' rl decoding line function, introduced test functions for the radolan reader

--- wradlib/tests/test_io.py
+++ wradlib/tests/test_io.py
@@ -3,6 +3,9 @@ import wradlib.io as io
 import wradlib.util as util
 import numpy as np
 import zlib
+import tempfile
+import os
+import datetime
 
 
 class IOTest(unittest.TestCase):
@@ -19,6 +22,110 @@ class IOTest(unittest.TestCase):
     def test_readDX(self):
         pass
 
+class RadolanTest(unittest.TestCase):
+
+    def test_get_radolan_header_token(self):
+        keylist = ['BY', 'VS', 'SW', 'PR', 'INT', 'GP',
+                   'MS', 'LV', 'CS', 'MX', 'BG']
+        head = io.get_radolan_header_token()
+        for key in keylist:
+            self.assertIsNone(head[key])
+    def test_get_token_position(self):
+        header = 'RW030950100000814BY1620130VS 3SW   2.13.1PR E-01INT  60GP 900x 900' \
+                 'MS 58<boo,ros,emd,hnr,pro,ess,asd,neu,nhb,oft,tur,isn,fbg,mem>'
+
+        test_head = io.get_radolan_header_token()
+        test_head['PR'] = (43,48)
+        test_head['GP'] = (57,66)
+        test_head['INT'] = (51,55)
+        test_head['SW'] = (32,41)
+        test_head['VS'] = (28,30)
+        test_head['MS'] = (68,None)
+        test_head['BY'] = (19,26)
+
+        head = io.get_token_position(header)
+        self.assertDictEqual(head, test_head)
+
+    def test_decode_radolan_runlength_line(self):
+        testarr = [ 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+          0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9., 9.,
+          9., 9., 9., 9., 9.]
+
+        testline = '\x10\x98\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xd9\n'
+        arr = np.frombuffer(testline, np.uint8).astype(np.uint8)
+        line = io.decode_radolan_runlength_line(arr)
+        self.assertTrue(np.allclose(line,testarr))
+
+    def test_read_radolan_runlength_line(self):
+        testline = '\x10\x98\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xf9\xd9\n'
+        testarr = np.frombuffer(testline, np.uint8).astype(np.uint8)
+        fid, temp_path = tempfile.mkstemp()
+        file = open(temp_path, 'w')
+        file.write(testline)
+        file.close()
+        file = open(temp_path, 'r')
+        line = io.read_radolan_runlength_line(file)
+        file.close()
+        os.close(fid)
+        os.remove(temp_path)
+        self.assertTrue(np.allclose(line, testarr))
+    def test_read_radolan_runlength_array(self):
+        pass
+
+    def test_parse_DWD_quant_composite_header(self):
+        rx_header = 'RW030950100000814BY1620130VS 3SW   2.13.1PR E-01INT  60GP 900x 900' \
+                 'MS 58<boo,ros,emd,hnr,pro,ess,asd,neu,nhb,oft,tur,isn,fbg,mem>'
+        test_rx = {'maxrange': '150 km', 'radarlocations': ['boo', 'ros', 'emd', 'hnr', 'pro',
+                                                            'ess', 'asd', 'neu', 'nhb', 'oft',
+                                                            'tur', 'isn', 'fbg', 'mem'],
+                   'nrow': 900, 'intervalseconds': 3600, 'precision': 0.1,
+                   'datetime': datetime.datetime(2014, 8, 3, 9, 50), 'ncol': 900,
+                   'radolanversion': '2.13.1', 'producttype': 'RW', 'radarid': '10000'}
+
+        pg_header = 'PG030905100000814BY20042LV 6  1.0 19.0 28.0 37.0 46.0 55.0CS0MX 0MS 82' \
+                    '<boo,ros,emd,hnr,pro,ess,asd,neu,nhb,oft,tur,isn,fbg,mem,czbrd> are used, ' \
+                    'BG460460'
+        test_pg = {'radarlocations': ['boo', 'ros', 'emd', 'hnr', 'pro', 'ess', 'asd', 'neu',
+                                      'nhb', 'oft', 'tur', 'isn', 'fbg', 'mem', 'czbrd'],
+                   'nrow': 460, 'level': [  1.,  19.,  28.,  37.,  46.,  55.],
+                   'datetime': datetime.datetime(2014, 8, 3, 9, 5), 'ncol': 460,
+                   'producttype': 'PG', 'radarid': '10000', 'nlevel': 6}
+
+        rx = io.parse_DWD_quant_composite_header(rx_header)
+        pg = io.parse_DWD_quant_composite_header(pg_header)
+
+        for key, value in rx.iteritems():
+            self.assertEqual(value, test_rx[key])
+        for key, value in pg.iteritems():
+            print(type(value))
+            if type(value) == np.ndarray:
+                self.assertTrue(np.allclose(value, test_pg[key]))
+            else:
+                self.assertEqual(value, test_pg[key])
+
+    def test_read_RADOLAN_composite(self):
+        pass
 
 class RainbowTest(unittest.TestCase):
     def test_read_rainbow(self):
