e48e0757d03b284afa29ec9871bf14b6299a6097
heisterm <heisterm@uni-potsdam.de>
2015-02-23 11:12:32 +0100

    ENH: Migrating module conversion from PyRadarMet to wradlib module trafo.
    This is is kind of a template for migrating code from the GitHub repository
    nguy/PyRadarMet to wradlib/wradlib. In this case, simple conversion functions
    for wind speed are migrated from PyRadarMet.conversion to wradlib.trafo. Here,
    we also examplify wradlib's testing strategies: We use doctests as well as
    one unittest per function.
    
    Please also note that we did not migrate the PyRadarMet functions ``dBZ2Z`` and
    ``Z2dBZ`` because these do the same as ``wradlib.trafo.idecibel`` and ``wradlib.trafo.decibel``,
    respectively, so there would be a conflict.
    
    Please also note that I had to rewrite the docstrings to make them compatible with wradlib.
    In addition, I renamed the function arguments to achieve some kind of harmonization.
    However, that should not change the behaviour as comopared to PyRadarMet.

--- wradlib/tests/test_trafo.py
+++ wradlib/tests/test_trafo.py
@@ -22,6 +22,10 @@ class TransformationTest(unittest.TestCase):
         self.lin = np.array([1e-4, 1, 1e4])
         self.dec = np.array([-40,0,40])
         self.r = np.array([5., 10., 20.])
+        self.speedsi  = np.array([0., 1.,       50.]) # speed in m/s
+        self.speedkmh = np.array([0., 3.6,      180.]) # speed in km/h
+        self.speedmph = np.array([0., 2.236932, 111.8466]) # speed in miles/h
+        self.speedkts = np.array([0., 0.51,     25.5]) # speed in knots
 
     def test_rvp2dBZ(self):
         self.assertTrue(np.allclose(trafo.rvp2dBZ(self.rvp), self.dbz))
@@ -35,6 +39,26 @@ class TransformationTest(unittest.TestCase):
     def test_r2depth(self):
         self.assertTrue(np.allclose(trafo.r2depth(self.r,720), np.array([1.,2.,4.])))
         self.assertTrue(np.allclose(trafo.r2depth(self.r,360), np.array([0.5,1.,2.])))
+
+    def test_si2kmh(self):
+        self.assertTrue(np.allclose(trafo.si2kmh(self.speedsi), self.speedkmh))
+
+    def test_si2mph(self):
+        self.assertTrue(np.allclose(trafo.si2mph(self.speedsi), self.speedmph))
+
+    def test_si2kts(self):
+        self.assertTrue(np.allclose(trafo.si2kts(self.speedsi), self.speedkts))
+
+    def test_kmh2si(self):
+        self.assertTrue(np.allclose(trafo.kmh2si(self.speedkmh), self.speedsi))
+
+    def test_mph2si(self):
+        self.assertTrue(np.allclose(trafo.mph2si(self.speedmph), self.speedsi))
+
+    def test_kts2si(self):
+        self.assertTrue(np.allclose(trafo.kts2si(self.speedkts), self.speedsi))
+
+
 
 if __name__ == '__main__':
     unittest.main()
