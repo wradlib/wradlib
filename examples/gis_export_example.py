237934b63e3dade6e480806270bbca4b3fcf00de
heisterm <heisterm@uni-potsdam.de>
2015-04-09 09:32:47 +0200

    FIX: Fixed a top-left corner issue with the RADOLAN grid in the GeoTIFF example.
    Plus some changes in the related docs.

--- examples/gis_export_example.py
+++ examples/gis_export_example.py
@@ -18,13 +18,15 @@ def ex_grid_to_gis():
     xy = wradlib.georef.get_radolan_grid(900,900)
     
     # Export to Arc/Info ASCII Grid format (aka ESRI grid)
-    # It should be possible to import this to most conventional GIS software.
-    wradlib.io.to_AAIGrid("data/aaigrid.asc", data, xy[0,0,0], xy[0,0,1], 1., proj=proj_osr)
-    print xy[0,0,0], xy[0,0,1]
-
+    #     It should be possible to import this to most conventional GIS software.
+    wradlib.io.to_AAIGrid("data/aaigrid.asc", data, xy[0,0,0], xy[0,0,1], 1., proj=proj_osr, to_esri=False)
+    
     # Export to GeoTIFF format
-    # For RADOLAN grids, this projection will probably not be recognized by ESRI ArcGIS.
-    geotransform = [xy[0,0,0], 1., 0, xy[-1,-1,1], 0, -1.]
+    #     For RADOLAN grids, this projection will probably not be recognized by ESRI ArcGIS.
+    #     Please note that that the geotransform for creating GeoTIFF files requires the
+    #     top-left corner of the bounding box. See help for io.to_GeoTIFF for further
+    #     instructions, particularly on how to define the geotransform.
+    geotransform = [xy[0,0,0], 1., 0, xy[-1,-1,1]+1., 0, -1.]
     wradlib.io.to_GeoTIFF("data/geotiff.tif", data, geotransform, proj=proj_osr)
 
 
