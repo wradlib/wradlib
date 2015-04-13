ea82ce3469eee55930490a6bfeb5c0852cdf2b42
heisterm <heisterm@uni-potsdam.de>
2015-04-13 16:04:58 +0200

    FIX: Fixing paths in gis_export_example.

--- examples/gis_export_example.py
+++ examples/gis_export_example.py
@@ -5,11 +5,14 @@
 #-------------------------------------------------------------------------------
 
 import wradlib
+import os
 
 def ex_grid_to_gis():
     
     # We will export this RADOLAN dataset to a GIS compatible format
-    data, meta = wradlib.io.read_RADOLAN_composite("data/radolan/raa01-sf_10000-1408102050-dwd---bin.gz")
+    wdir = os.path.dirname(__file__) + '/data/radolan/'
+    fpath = wdir + 'raa01-sf_10000-1408102050-dwd---bin.gz'
+    data, meta = wradlib.io.read_RADOLAN_composite(fpath)
     
     # This is the RADOLAN projection
     proj_osr = wradlib.georef.create_osr( "dwd-radolan" )
@@ -19,7 +22,7 @@ def ex_grid_to_gis():
     
     # Export to Arc/Info ASCII Grid format (aka ESRI grid)
     #     It should be possible to import this to most conventional GIS software.
-    wradlib.io.to_AAIGrid("data/aaigrid.asc", data, xy[0,0,0], xy[0,0,1], 1., proj=proj_osr, to_esri=False)
+    wradlib.io.to_AAIGrid(wdir+"aaigrid.asc", data, xy[0,0,0], xy[0,0,1], 1., proj=proj_osr, to_esri=False)
     
     # Export to GeoTIFF format
     #     For RADOLAN grids, this projection will probably not be recognized by ESRI ArcGIS.
@@ -27,7 +30,7 @@ def ex_grid_to_gis():
     #     top-left corner of the bounding box. See help for io.to_GeoTIFF for further
     #     instructions, particularly on how to define the geotransform.
     geotransform = [xy[0,0,0], 1., 0, xy[-1,-1,1]+1., 0, -1.]
-    wradlib.io.to_GeoTIFF("data/geotiff.tif", data, geotransform, proj=proj_osr)
+    wradlib.io.to_GeoTIFF(wdir+"geotiff.tif", data, geotransform, proj=proj_osr)
 
 
 # =======================================================
