e365f1bea2546474cb796085d57bd6b5f7ea108b
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2015-04-30 14:14:40 +0200

    DOC: fix intersphinx mapping for matplotlib

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -250,7 +250,7 @@ intersphinx_mapping = {
     'python': ('http://docs.python.org/2', None),
     'numpy': ('http://docs.scipy.org/doc/numpy/', None),
     'scipy': ('http://docs.scipy.org/doc/scipy/reference/', None),
-    'matplotlib': ('http://matplotlib.sourceforge.net/', None),
+    'matplotlib': ('http://matplotlib.org/', None),
     'sphinx': ('http://sphinx-doc.org', None),
 }
 
