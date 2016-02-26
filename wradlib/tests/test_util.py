f07564475b4a10c8573a6ede76070155f4b4bdee
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2016-02-26 09:45:00 +0100

    WIP: add more tests to test_util.py, fix two minor issues in util.py and add MIP example to plot_cg_ppi_example.py

--- wradlib/tests/test_util.py
+++ wradlib/tests/test_util.py
@@ -1,13 +1,55 @@
 import numpy as np
 import wradlib.util as util
 import unittest
+import datetime as dt
+
+
+class HelperFunctionsTest(unittest.TestCase):
+    def test__get_func(self):
+        self.assertEqual(util._get_func('arange').__class__, np.arange.__class__)
+        self.assertEqual(util._get_func('arange').__module__, np.arange.__module__)
+        self.assertRaises(AttributeError, lambda: util._get_func('aranged'))
+
+    def test__shape2size(self):
+        self.assertEqual(util._shape2size((10, 10, 10)), 10*10*10)
+
+    def test__tdelta2seconds(self):
+        self.assertEqual(util._tdelta2seconds(dt.datetime(2001,1,1,1)-dt.datetime(2000,1,1)), 366 * 24 * 60 * 60 + 3600)
+        self.assertEqual(util._tdelta2seconds(dt.datetime(2002,1,1,1)-dt.datetime(2001,1,1)), 365 * 24 * 60 * 60 + 3600)
+
+    def test__get_tdelta(self):
+        tstart = dt.datetime(2000,1,1)
+        tend = dt.datetime(2001,1,1,1)
+        tstart_str = "2000-01-01 00:00:00"
+        tend_str = "2001-01-01 01:00:00"
+        self.assertEqual(util._get_tdelta(tstart, tend), tend-tstart)
+        self.assertEqual(util._get_tdelta(tstart_str, tend_str), tend-tstart)
+        self.assertRaises(ValueError, lambda: util._get_tdelta(tstart_str, tend_str+".00000"))
+        self.assertEqual(util._get_tdelta(tstart_str, tend_str, as_secs=True), 366 * 24 * 60 * 60 + 3600)
+
+    def test__idvalid(self):
+        data = np.array([np.inf, np.nan, -99., 99, -9999., -9999, -10., -5., 0., 5., 10.])
+        self.assertTrue(np.allclose(util._idvalid(data), np.array([6, 7, 8, 9, 10])))
+        self.assertTrue(np.allclose(util._idvalid(data, minval=-5., maxval=5.), np.array([7, 8, 9])))
+        self.assertTrue(np.allclose(util._idvalid(data, isinvalid=[-9999], maxval=5.), np.array([2, 6, 7, 8, 9])))
+
+    def test_issequence(self):
+        self.assertTrue(util.issequence([0, 1, 2]))
+        self.assertFalse(util.issequence(1))
+        self.assertFalse(util.issequence('str'))
+
+    def test_trapezoid(self):
+        pass
+
+    def test_prob_round(self):
+        pass
 
 
 # -------------------------------------------------------------------------------
 # testing the filter helper function
 # -------------------------------------------------------------------------------
 class TestUtil(unittest.TestCase):
-    def img_setup(self):
+    def setUp(self):
         img = np.zeros((36, 10), dtype=np.float32)
         img[2, 2] = 1  # isolated pixel
         img[5, 6:8] = 1  # line
@@ -15,10 +57,8 @@ class TestUtil(unittest.TestCase):
         img[9:12, 4:7] = 1  # precip field
         # img[15:17,5:7] = np.nan # nodata as nans
         self.img = img
-        pass
 
     def test_filter_window_polar(self):
-        self.img_setup()
         np.set_printoptions(precision=3)
         rscale = 250
         nrays, nbins = self.img.shape
@@ -69,6 +109,47 @@ class TestUtil(unittest.TestCase):
                         523.599, 610.865, 698.132, 785.398])
         self.assertTrue(np.allclose(hpr, res))
 
+    def test_filter_window_cartesian(self):
+        #print(self.img)
+        print(util.filter_window_cartesian(self.img, 500., "maximum", np.array([250., 250])))
+        correct = np.array([[0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 1., 1., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 1., 1., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 1., 1., 1., 0.],
+                            [0., 0., 0., 0., 0., 0., 1., 1., 1., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 1., 1., 1., 1., 0., 0.],
+                            [0., 0., 0., 0., 1., 1., 1., 1., 0., 0.],
+                            [0., 0., 0., 0., 1., 1., 1., 1., 0., 0.],
+                            [0., 0., 0., 0., 1., 1., 1., 1., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+                            [1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                            [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]])
+        self.assertTrue(np.allclose(util.filter_window_cartesian(self.img, 500., "maximum", np.array([250., 250])),
+                                    correct))
 
 class FindBboxIndicesTest(unittest.TestCase):
     def setUp(self):
