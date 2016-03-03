8e6ab0bf9d3ca519d5497f90fa3490910ddb8833
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2016-03-03 09:22:30 +0100

    MIN: add test_fill.py stub adn minor changes in __init__.py

---
+++ wradlib/tests/test_fill.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -------------------------------------------------------------------------------
+# Name:        test_fill.py
+# Purpose:     testing file for the wradlib.fill module
+#
+# Authors:     wradlib developers
+#
+# Created:     03.03.2016
+# Copyright:   (c) wradlib developers
+# Licence:     The MIT License
+# -------------------------------------------------------------------------------
+
+import unittest
+
+import wradlib.fill as fill
+
+if __name__ == '__main__':
+    unittest.main()
