cae0230c64f6abc5e478cb0aff8770dea1539c30
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2016-04-08 21:26:32 +0200

    WIP: recreate PATH

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
     -O miniconda.sh
 chmod +x miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
-export PATH=""#/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:"
 export PATH="$HOME/miniconda/bin:$PATH"
 #export PATH=/home/travis/miniconda2/bin:$PATH
 #export PATH=/home/k.muehlbauer/miniconda2/bin:$PATH
