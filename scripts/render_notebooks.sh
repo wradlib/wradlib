df50a7067ef45ef4de28ef88a13bb438591f5d88
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2016-04-20 09:35:47 +0200

    ENH: split process_notebook.sh into convert_notebook.sh and render_notebook.sh

--- scripts/render_notebooks.sh
+++ scripts/render_notebooks.sh
@@ -2,11 +2,6 @@
 # Copyright (c) 2016, wradlib developers.
 # Distributed under the MIT License. See LICENSE.txt for more info.
 
-# convert notebooks to python scripts
-cd notebooks
-jupyter nbconvert --to script *.ipynb
-cd ..
-
 # render notebooks to doc/sources
 notebooks=`ls notebooks/*.ipynb`
 echo $notebooks
