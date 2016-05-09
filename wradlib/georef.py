FIX: fix issue #60, `ascale` missing factor 2

wradlib/georef.py
@@ -1265,7 +1265,7 @@ def sweep_centroids(nrays, rscale, nbins, elangle):
         array of shape (nrays,nbins,3) containing native centroid radar
         coordinates (slant range, azimuth, elevation)
     """
-    ascale = np.pi / nrays
+    ascale = 2 * np.pi / nrays
     azimuths = ascale / 2. + np.linspace(0, 2 * np.pi, nrays, endpoint=False)
     ranges = np.arange(nbins) * rscale + rscale / 2.
     coordinates = np.empty((nrays, nbins, 3), dtype=float)