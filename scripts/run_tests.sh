09ab23675de5d989f05ece615041120a8db48120
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2017-03-10 07:45:44 +0100

    WIP: run coverage for the whole build matrix, codecov will combine automatically

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2017, wradlib developers.
 # Distributed under the MIT License. See LICENSE.txt for more info.
 
-if [[ "$COVERALLS" == "true" ]]; then
+if [[ "$COVERAGE" == "true" ]]; then
     xvfb-run -a -w 5 coverage run --source wradlib testrunner.py -u
     xvfb-run -a -w 5 coverage run -a --source wradlib testrunner.py -d
     xvfb-run -a -w 5 coverage run -a --source wradlib testrunner.py -e
