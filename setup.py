ebf2faffcc2c8392f0a12af8361bbd1eb2db45c9
Kai Mühlbauer <kmuehlbauer@users.noreply.github.com>
2017-04-10 08:27:57 +0200

    MAINT: bump release number
    bump release number to 0.10.0, set `ISRELEASED=True`

--- setup.py
+++ setup.py
@@ -48,9 +48,9 @@ LICENSE = 'MIT'
 CLASSIFIERS = filter(None, CLASSIFIERS.split('\n'))
 PLATFORMS = ["Linux", "Mac OS-X", "Unix", "Windows"]
 MAJOR = 0
-MINOR = 9
-MICRO = 1
-ISRELEASED = False
+MINOR = 10
+MICRO = 0
+ISRELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
