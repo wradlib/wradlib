56ec80d13c9861b0c4fc5566d146e607db312276
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2017-06-20 09:02:11 +0200

    WIP: add `wradlib.io.gdal`, fix file headers

--- wradlib/io/__init__.py
+++ wradlib/io/__init__.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# Copyright (c) 2011-2017, wradlib developers.
+# Distributed under the MIT License. See LICENSE.txt for more info.
+
 """
 Raw Data I/O
 ============
@@ -13,11 +17,11 @@ for an introduction on how to deal with different file formats.
 .. automodule:: wradlib.io.hdf
 .. automodule:: wradlib.io.netcdf
 .. automodule:: wradlib.io.rainbow
+.. automodule:: wradlib.io.gdal
 .. automodule:: wradlib.io.misc
 """
-from .misc import (writePolygon2Text, read_safnwc, to_pickle,  # noqa
-                   from_pickle, write_raster_dataset, open_shape,  # noqa
-                   open_raster)  # noqa
+
+from .misc import (writePolygon2Text,  to_pickle, from_pickle)  # noqa
 from .hdf import (read_generic_hdf5, read_OPERA_hdf5, read_GAMIC_hdf5,  # noqa
                   to_hdf5, from_hdf5)  # noqa
 from .netcdf import read_EDGE_netcdf, read_generic_netcdf  # noqa
@@ -25,4 +29,6 @@ from .rainbow import read_Rainbow  # noqa
 from .radolan import (readDX, read_RADOLAN_composite,  # noqa
                       read_radolan_header,  get_radolan_filehandle,  # noqa
                       parse_DWD_quant_composite_header)  # noqa
+from .gdal import (read_safnwc, write_raster_dataset,  # noqa
+                   open_shape, open_raster)  # noqa
 __all__ = [s for s in dir() if not s.startswith('_')]
