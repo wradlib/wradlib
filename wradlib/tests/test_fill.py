26d348a3d4cbd44364020170391b2794889665fe
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2018-03-14 16:07:04 +0100

    ENH: remove `fill`

--- wradlib/tests/test_fill.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# Copyright (c) 2011-2018, wradlib developers.
-# Distributed under the MIT License. See LICENSE.txt for more info.
-
-import unittest
-
-if __name__ == '__main__':
-    unittest.main()
