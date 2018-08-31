6e68aef91946c8e47f5a3a975b9e937500ecc488
Kai Mühlbauer <kmuehlbauer@users.noreply.github.com>
2018-08-31 13:06:20 +0200

    MIN: remove unneeded secret from .travis.yml, add rtd webhook (#280)
    * MIN: remove unneeded secret from .travis.yml, add rtd webhook
    * MIN: move rtd webhook trigger into dedicated shell-script

---
+++ scripts/trigger_readthedocs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright (c) 2011-2018, wradlib developers.
+# Distributed under the MIT License. See LICENSE.txt for more info.
+
+set -e
+
+if [ "$TRAVIS_PYTHON_VERSION" = "3.6" ] && ["$TRAVIS_PULL_REQUEST" = "false" ] ; then
+    curl -X POST -d "token=$RTD_TOKEN" $RTD_URL
+fi
