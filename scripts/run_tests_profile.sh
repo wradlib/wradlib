00ee2faa26eca3228254563d98a2b7270fdd60c5
egouden <egouden@gmail.com>
2019-05-10 09:32:45 +0200

    Feature/scriptprofiling (#325)
    * new script for test profiling
    * adding example

---
+++ scripts/run_tests_profile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (c) 2011-2019, wradlib developers.
+# Distributed under the MIT License. See LICENSE.txt for more info.
+# Example:run_tests_profile.sh "wradlib.tests.test_io" "XarrayTests().test_read_odim()" 
+
+set -e
+
+module=$1
+method=$2
+
+python -c "import $module ; import cProfile ; cProfile.run('${module}.${method}','restats')"
+python -c "import pstats; pstats.Stats('restats').sort_stats('cumtime').print_stats(50)"
+rm restats
