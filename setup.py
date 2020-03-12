59a4ffa9bd6e7a3833207d38cbb1dba468597b68
Kai Mühlbauer <kmuehlbauer@users.noreply.github.com>
2020-03-12 10:11:24 +0100

    MNT: Rel 1.6.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ LICENSE = 'MIT'
 CLASSIFIERS = filter(None, CLASSIFIERS.split('\n'))
 PLATFORMS = ["Linux", "Mac OS-X", "Unix", "Windows"]
 MAJOR = 1
-MINOR = 5
+MINOR = 6
 PATCH = 0
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, PATCH)
 
