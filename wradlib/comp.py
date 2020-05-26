fcb6a1af537e21fb44c22ee98b1cb01c76d88da1
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2020-05-26 11:05:22 +0200

    FIX: docstring

--- wradlib/comp.py
+++ wradlib/comp.py
@@ -79,7 +79,7 @@ def togrid(src, trg, radius, center, data, interpol, *args, **kwargs):
     Note
     ----
     Keyword arguments to be used while calling the interpolator can be issued as
-    `call_kwargs`, eg. togrid(..... 'call_kwargs=dict(maxdist=10)')
+    `call_kwargs`, eg. togrid(..., call_kwargs=dict(maxdist=10))
 
     Examples
     --------
