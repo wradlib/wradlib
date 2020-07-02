9bb15f012c141bd1a2b05df3388b72466129d8a5
Kai Mühlbauer <kmuehlbauer@users.noreply.github.com>
2020-07-02 15:10:11 +0200

    MNT: enhance ci workflow (#436)
    * MNT: add flake8 linter to azure ci
    * MNT: update README.md
    * MNT: upload coverage on azure ci
    * MNT: upload coverage on azure ci for notebook tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,7 +7,9 @@ set -e
 if [[ "$TRAVIS" == "true" ]]; then
     # export location of .coveragerc
     export COVERAGE_PROCESS_START=$WRADLIB_BUILD_DIR/.coveragerc
-    pytest --verbose --doctest-modules --durations=15 --cov-report xml:coverage.xml --cov=wradlib --pyargs $1
+    coverage run -m pytest --verbose --doctest-modules --durations=15 --pyargs $1
+    coverage combine
+    coverage xml
 else
     pytest --verbose --doctest-modules --durations=15 --pyargs $1
 fi
