f0b4b5d4d1c821beceef2130bfed866e6a79504a
Kai Mühlbauer <kmuehlbauer@users.noreply.github.com>
2021-03-02 08:30:44 +0100

    MNT: Rel. 1.9.1

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ CLASSIFIERS = list(filter(None, CLASSIFIERS.split("\n")))
 PLATFORMS = ["Linux", "Mac OS-X", "Unix", "Windows"]
 MAJOR = 1
 MINOR = 9
-PATCH = 0
+PATCH = 1
 VERSION = "%d.%d.%d" % (MAJOR, MINOR, PATCH)
 
 
