1c7ec6a797bfbb1c3375c8f3681e47170d418797
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2021-09-30 08:02:03 +0200

    FIX: use tolerance in degree, do not combine with resolution

--- wradlib/io/xarray.py
+++ wradlib/io/xarray.py
@@ -1526,11 +1526,10 @@ def _reindex_angle(ds, store=None, force=False, tol=None):
                 for k, v in ds.items()
                 if hasattr(v, "_FillValue")
             }
-            # todo: make tolerance parameterizable
             ds = ds.reindex(
                 {dimname: azr},
                 method="nearest",
-                tolerance=res * tol,
+                tolerance=tol,
                 fill_value=fill_value,
             )
 
