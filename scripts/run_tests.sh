7dbaada81f185eb8aa7984c962e108beea644aa1
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2021-11-11 08:09:55 +0100

    MNT: use pytest-doctestplus in scripts/CI

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,9 +7,9 @@ set -e
 if [[ "$TRAVIS" == "true" ]]; then
     # export location of .coveragerc
     export COVERAGE_PROCESS_START=$WRADLIB_BUILD_DIR/.coveragerc
-    coverage run -m pytest --verbose --doctest-modules --durations=15 --pyargs $1
+    coverage run -m pytest --verbose --doctest-modules --doctest-plus --durations=15 --pyargs $1
     coverage combine
     coverage xml
 else
-    pytest --verbose --doctest-modules --durations=15 --pyargs $1
+    pytest --verbose --doctest-modules --doctest-plus --durations=15 --pyargs $1
 fi
