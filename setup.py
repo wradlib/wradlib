b13e6808dd922ee1e96c3b406b635644ef563c61
Kai Muehlbauer <kmuehlbauer@wradlib.org>
2022-03-17 15:58:05 +0100

    MNT: rework build-system
    Signed-off-by: Kai Muehlbauer <kmuehlbauer@wradlib.org>

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2011-2021, wradlib developers.
+# Copyright (c) 2011-2022, wradlib developers.
 # Distributed under the MIT License. See LICENSE.txt for more info.
 
 """wradlib - An Open Source Library for Weather Radar Data Processing
@@ -11,12 +11,11 @@ intensity, identifying and correcting typical error sources (such as
 clutter or attenuation) and visualising the data.
 """
 
-import builtins
-import os
-import warnings
-from subprocess import CalledProcessError, check_output
+import sys
+from setuptools import find_packages, setup
 
-import semver
+if sys.version_info[:2] < (3, 6):
+    raise RuntimeError("Python version >= 3.6 required.")
 
 DOCLINES = __doc__.split("\n")
 
@@ -32,6 +31,7 @@ Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
 Topic :: Scientific/Engineering
 Topic :: Scientific/Engineering :: Atmospheric Science
 Operating System :: POSIX :: Linux
@@ -44,131 +44,13 @@ MAINTAINER = "wradlib developers"
 MAINTAINER_EMAIL = "wradlib@wradlib.org"
 DESCRIPTION = DOCLINES[0]
 LONG_DESCRIPTION = "\n".join(DOCLINES[2:])
-URL = "http://wradlib.org"
+URL = "https://wradlib.org"
 DOWNLOAD_URL = "https://github.com/wradlib/wradlib"
 LICENSE = "MIT"
 CLASSIFIERS = list(filter(None, CLASSIFIERS.split("\n")))
 PLATFORMS = ["Linux", "Mac OS-X", "Unix", "Windows"]
-MAJOR = 1
-MINOR = 13
-PATCH = 0
-VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
-
-
-# Return the git revision as a string
-def git_version():
-    try:
-        git_rev = check_output(["git", "describe", "--tags", "--long"])
-        git_hash = check_output(["git", "rev-parse", "HEAD"])
-        branch = check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
-
-        git_rev = git_rev.strip().decode("ascii").split("-")
-        branch = branch.strip().decode("ascii")
-
-        GIT_REVISION = "-".join([git_rev[0], "dev" + git_rev[1]])
-        GIT_REVISION = "+".join([GIT_REVISION, git_rev[2]])
-        GIT_HASH = git_hash.strip().decode("ascii")
-
-        ver = semver.VersionInfo.parse(GIT_REVISION)
-        minor = ver.minor
-        patch = ver.patch
-
-        if ver.prerelease != "dev0":
-            if "release" not in branch:
-                minor += 1
-            else:
-                patch += 1
-        GIT_REVISION = semver.format_version(
-            ver.major, minor, patch, ver.prerelease, ver.build
-        )
-
-    except (CalledProcessError, OSError):
-        print("WRADLIB: Unable to import git_revision from repository.")
-        raise
-    return GIT_REVISION, GIT_HASH
-
-
-# This is a bit hackish: we are setting a global variable so that the main
-# wradlib __init__ can detect if it is being loaded by the setup routine, to
-# avoid attempting to load components that aren't built yet.
-builtins.__WRADLIB_SETUP__ = True
-
-
-def write_version_py(filename="wradlib/version.py"):
-    cnt = """
-# THIS FILE IS GENERATED FROM WRADLIB SETUP.PY
-short_version = '%(short_version)s'
-version = '%(version)s'
-full_version = '%(full_version)s'
-git_revision = '%(git_revision)s'
-release = %(isrelease)s
-"""
-    # Adding the git rev number needs to be done inside write_version_py(),
-    # otherwise the import of wradlib.version messes up the build under
-    # Python 3.
-
-    SHORT_VERSION = VERSION
-    FULL_VERSION = VERSION
-    GIT_REVISION = VERSION + "-unknown"
-    GIT_HASH = "unknown"
-    ISRELEASED = "'unknown'"
-
-    if os.path.exists(".git"):
-        GIT_REVISION, GIT_HASH = git_version()
-    elif os.path.exists("wradlib/version.py"):
-        # must be a source distribution, use existing version file
-        try:
-            from wradlib.version import full_version as GIT_REVISION
-            from wradlib.version import git_revision as GIT_HASH
-        except ImportError:
-            raise ImportError(
-                "Unable to import git_revision. Try removing "
-                "wradlib/version.py and the build directory "
-                "before building."
-            )
-    else:
-        warnings.warn(
-            "wradlib source does not contain detailed version info "
-            "via git or version.py, exact version can't be "
-            "retrieved.",
-            UserWarning,
-        )
-
-    # parse version using semver
-    ver = semver.VersionInfo.parse(GIT_REVISION)
-
-    # get commit count, dev0 means tagged commit -> release
-    ISRELEASED = ver.prerelease == "dev0"
-    if not ISRELEASED:
-        SHORT_VERSION = semver.format_version(
-            ver.major, ver.minor, ver.patch, ver.prerelease
-        )
-        FULL_VERSION = GIT_REVISION
-
-    a = open(filename, "w")
-    try:
-        a.write(
-            cnt
-            % {
-                "short_version": SHORT_VERSION,
-                "version": FULL_VERSION,
-                "full_version": GIT_REVISION,
-                "git_revision": GIT_HASH,
-                "isrelease": str(ISRELEASED),
-            }
-        )
-    finally:
-        a.close()
-
-    return SHORT_VERSION
-
 
 def setup_package():
-
-    # rewrite version file
-    VERSION = write_version_py()
-
-    from setuptools import find_packages, setup
 
     with open("requirements.txt", "r") as f:
         INSTALL_REQUIRES = [rq for rq in f.read().split("\n") if rq != ""]
@@ -188,11 +70,11 @@ def setup_package():
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         url=URL,
-        version=VERSION,
         download_url=DOWNLOAD_URL,
         license=LICENSE,
         classifiers=CLASSIFIERS,
         platforms=PLATFORMS,
+        setup_requires=["setuptools_scm"],
         install_requires=INSTALL_REQUIRES,
         extras_require=dict(
             dev=DEVEL_REQUIRES,
--- wradlib/__init__.py
+++ wradlib/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2011-2020, wradlib developers.
+# Copyright (c) 2011-2022, wradlib developers.
 # Distributed under the MIT License. See LICENSE.txt for more info.
 
 """
@@ -9,45 +9,32 @@ wradlib
 isort:skip_file
 """
 
-# Detect if we're being called as part of wradlib's setup procedure
-try:
-    __WRADLIB_SETUP__
-except NameError:
-    __WRADLIB_SETUP__ = False
+# Make sure that deprecation warnings get printed by default
+import warnings as _warnings
 
-if __WRADLIB_SETUP__:
-    import sys as _sys
+_warnings.filterwarnings("always", category=DeprecationWarning, module="wradlib")
 
-    _sys.stderr.write("Running from wradlib source directory.\n")
-    del _sys
-else:
-    # Make sure that deprecation warnings get printed by default
-    import warnings as _warnings
+# versioning
+from .version import version as __version__  # noqa
 
-    _warnings.filterwarnings("always", category=DeprecationWarning, module="wradlib")
+# import subpackages
+from . import adjust  # noqa
+from . import atten  # noqa
+from . import classify  # noqa
+from . import clutter  # noqa
+from . import comp  # noqa
+from . import dp  # noqa
+from . import georef  # noqa
+from . import io  # noqa
+from . import ipol  # noqa
+from . import qual  # noqa
+from . import trafo  # noqa
+from . import util  # noqa
+from . import verify  # noqa
+from . import vis  # noqa
+from . import vpr  # noqa
+from . import zonalstats  # noqa
+from . import zr  # noqa
+from .util import show_versions  # noqa
 
-    # versioning
-    from .version import git_revision as __git_revision__  # noqa
-    from .version import version as __version__  # noqa
-
-    # import subpackages
-    from . import adjust  # noqa
-    from . import atten  # noqa
-    from . import classify  # noqa
-    from . import clutter  # noqa
-    from . import comp  # noqa
-    from . import dp  # noqa
-    from . import georef  # noqa
-    from . import io  # noqa
-    from . import ipol  # noqa
-    from . import qual  # noqa
-    from . import trafo  # noqa
-    from . import util  # noqa
-    from . import verify  # noqa
-    from . import vis  # noqa
-    from . import vpr  # noqa
-    from . import zonalstats  # noqa
-    from . import zr  # noqa
-    from .util import show_versions  # noqa
-
-    __all__ = [s for s in dir() if not s.startswith("_")]
+__all__ = [s for s in dir() if not s.startswith("_")]
