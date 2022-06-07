3d35f48e632287f01317dbf7ae90e735e977d60a
syedhamidali <hamidsyed37@gmail.com>
2022-06-07 15:34:07 -0400

    update ipol.py

--- wradlib/ipol.py
+++ wradlib/ipol.py
@@ -1579,7 +1579,7 @@ def interpolate_polar(data, mask=None, ipclass=Nearest):
     ranges = np.tile(np.arange(0.5, data.shape[1] + 0.5), data.shape[0])
     # construct the angles for every bin
     angles = np.repeat(
-        np.radians(np.linspace(0,360,endpoint=False, num = data.shape[0])), data.shape[1]
+        np.radians(np.linspace(0, 360, endpoint = False, num = data.shape[0])), data.shape[1]
         )
     # calculate cartesian coordinates for every bin
     binx = np.cos(angles) * ranges
