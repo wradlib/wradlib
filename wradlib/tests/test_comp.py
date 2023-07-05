ebef47e90ceb93a20976efedc21a0b30a6ce380f
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2023-07-05 11:02:09 +0200

    ENH: update comp-module/tests

--- wradlib/tests/test_comp.py
+++ wradlib/tests/test_comp.py
@@ -33,217 +33,218 @@ def comp_data():
     yield Data
 
 
-class TestCompose:
-    @requires_data
-    @requires_gdal
-    def test_extract_circle(self, comp_data):
-        x = comp_data.x
-        y = comp_data.y
-        xgrid = np.linspace(x.min(), x.mean(), 100)
-        ygrid = np.linspace(y.min(), y.mean(), 100)
-        grid_xy = np.meshgrid(xgrid, ygrid)
-        grid_xy = np.vstack((grid_xy[0].ravel(), grid_xy[1].ravel())).transpose()
-        comp.extract_circle(np.array([x.mean(), y.mean()]), 128000.0, grid_xy)
+@requires_data
+@requires_gdal
+def test_extract_circle(comp_data):
+    x = comp_data.x
+    y = comp_data.y
+    xgrid = np.linspace(x.min(), x.mean(), 100)
+    ygrid = np.linspace(y.min(), y.mean(), 100)
+    grid_xy = np.meshgrid(xgrid, ygrid)
+    grid_xy = np.vstack((grid_xy[0].ravel(), grid_xy[1].ravel())).transpose()
+    comp.extract_circle(np.array([x.mean(), y.mean()]), 128000.0, grid_xy)
 
-    @requires_data
-    @requires_gdal
-    def test_togrid(self, comp_data):
-        x = comp_data.x
-        y = comp_data.y
-        data = comp_data.data
-        xgrid = np.linspace(x.min(), x.mean(), 100)
-        ygrid = np.linspace(y.min(), y.mean(), 100)
-        grid_xy = np.meshgrid(xgrid, ygrid)
-        grid_xy = np.vstack((grid_xy[0].ravel(), grid_xy[1].ravel())).transpose()
-        xy = np.concatenate([x.ravel()[:, None], y.ravel()[:, None]], axis=1)
-        comp.togrid(
-            xy,
-            grid_xy,
-            128000.0,
-            np.array([x.mean(), y.mean()]),
-            data.ravel(),
-            ipol.Nearest,
-        )
 
-    def test_compose(self):
-        g1 = np.array(
-            [
-                np.nan,
-                np.nan,
-                10.0,
-                np.nan,
-                np.nan,
-                np.nan,
-                10.0,
-                10.0,
-                10.0,
-                np.nan,
-                10.0,
-                10.0,
-                10.0,
-                10.0,
-                np.nan,
-                np.nan,
-                10.0,
-                10.0,
-                10.0,
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-            ]
-        )
-        g2 = np.array(
-            [
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-                11.0,
-                11.0,
-                11.0,
-                np.nan,
-                np.nan,
-                11.0,
-                11.0,
-                11.0,
-                11.0,
-                np.nan,
-                11.0,
-                11.0,
-                11.0,
-                np.nan,
-                np.nan,
-                np.nan,
-                11.0,
-                np.nan,
-                np.nan,
-            ]
-        )
-        q1 = np.array(
-            [
-                np.nan,
-                np.nan,
-                3.47408756e09,
-                np.nan,
-                np.nan,
-                np.nan,
-                8.75744493e08,
-                8.75744493e08,
-                1.55045236e09,
-                np.nan,
-                3.47408756e09,
-                8.75744493e08,
-                5.98145272e04,
-                1.55045236e09,
-                np.nan,
-                np.nan,
-                1.55045236e09,
-                1.55045236e09,
-                1.55045236e09,
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-            ]
-        )
-        q2 = np.array(
-            [
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-                np.nan,
-                1.55045236e09,
-                1.55045236e09,
-                1.55045236e09,
-                np.nan,
-                np.nan,
-                1.55045236e09,
-                5.98145272e04,
-                8.75744493e08,
-                3.47408756e09,
-                np.nan,
-                1.55045236e09,
-                8.75744493e08,
-                8.75744493e08,
-                np.nan,
-                np.nan,
-                np.nan,
-                3.47408756e09,
-                np.nan,
-                np.nan,
-            ]
-        )
+@requires_data
+@requires_gdal
+def test_togrid(comp_data):
+    x = comp_data.x
+    y = comp_data.y
+    data = comp_data.data
+    xgrid = np.linspace(x.min(), x.mean(), 100)
+    ygrid = np.linspace(y.min(), y.mean(), 100)
+    grid_xy = np.meshgrid(xgrid, ygrid)
+    grid_xy = np.vstack((grid_xy[0].ravel(), grid_xy[1].ravel())).transpose()
+    xy = np.concatenate([x.ravel()[:, None], y.ravel()[:, None]], axis=1)
+    comp.togrid(
+        xy,
+        grid_xy,
+        128000.0,
+        np.array([x.mean(), y.mean()]),
+        data.ravel(),
+        ipol.Nearest,
+    )
 
-        composite = comp.compose_weighted(
-            [g1, g2], [1.0 / (q1 + 0.001), 1.0 / (q2 + 0.001)]
-        )
-        composite1 = comp.compose_ko([g1, g2], [1.0 / (q1 + 0.001), 1.0 / (q2 + 0.001)])
-        res = np.array(
-            [
-                np.nan,
-                np.nan,
-                10.0,
-                np.nan,
-                np.nan,
-                np.nan,
-                10.3609536,
-                10.3609536,
-                10.5,
-                np.nan,
-                10.0,
-                10.3609536,
-                10.5,
-                10.6390464,
-                11.0,
-                np.nan,
-                10.5,
-                10.6390464,
-                10.6390464,
-                np.nan,
-                np.nan,
-                np.nan,
-                11.0,
-                np.nan,
-                np.nan,
-            ]
-        )
-        res1 = np.array(
-            [
-                np.nan,
-                np.nan,
-                10.0,
-                np.nan,
-                np.nan,
-                np.nan,
-                10.0,
-                10.0,
-                10.0,
-                np.nan,
-                10.0,
-                10.0,
-                10.0,
-                11.0,
-                11.0,
-                np.nan,
-                10.0,
-                11.0,
-                11.0,
-                np.nan,
-                np.nan,
-                np.nan,
-                11.0,
-                np.nan,
-                np.nan,
-            ]
-        )
-        np.testing.assert_allclose(composite, res)
-        np.testing.assert_allclose(composite1, res1)
+
+def test_compose():
+    g1 = np.array(
+        [
+            np.nan,
+            np.nan,
+            10.0,
+            np.nan,
+            np.nan,
+            np.nan,
+            10.0,
+            10.0,
+            10.0,
+            np.nan,
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+            np.nan,
+            np.nan,
+            10.0,
+            10.0,
+            10.0,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+        ]
+    )
+    g2 = np.array(
+        [
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            11.0,
+            11.0,
+            11.0,
+            np.nan,
+            np.nan,
+            11.0,
+            11.0,
+            11.0,
+            11.0,
+            np.nan,
+            11.0,
+            11.0,
+            11.0,
+            np.nan,
+            np.nan,
+            np.nan,
+            11.0,
+            np.nan,
+            np.nan,
+        ]
+    )
+    q1 = np.array(
+        [
+            np.nan,
+            np.nan,
+            3.47408756e09,
+            np.nan,
+            np.nan,
+            np.nan,
+            8.75744493e08,
+            8.75744493e08,
+            1.55045236e09,
+            np.nan,
+            3.47408756e09,
+            8.75744493e08,
+            5.98145272e04,
+            1.55045236e09,
+            np.nan,
+            np.nan,
+            1.55045236e09,
+            1.55045236e09,
+            1.55045236e09,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+        ]
+    )
+    q2 = np.array(
+        [
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            1.55045236e09,
+            1.55045236e09,
+            1.55045236e09,
+            np.nan,
+            np.nan,
+            1.55045236e09,
+            5.98145272e04,
+            8.75744493e08,
+            3.47408756e09,
+            np.nan,
+            1.55045236e09,
+            8.75744493e08,
+            8.75744493e08,
+            np.nan,
+            np.nan,
+            np.nan,
+            3.47408756e09,
+            np.nan,
+            np.nan,
+        ]
+    )
+
+    composite = comp.compose_weighted(
+        [g1, g2], [1.0 / (q1 + 0.001), 1.0 / (q2 + 0.001)]
+    )
+    composite1 = comp.compose_ko([g1, g2], [1.0 / (q1 + 0.001), 1.0 / (q2 + 0.001)])
+    res = np.array(
+        [
+            np.nan,
+            np.nan,
+            10.0,
+            np.nan,
+            np.nan,
+            np.nan,
+            10.3609536,
+            10.3609536,
+            10.5,
+            np.nan,
+            10.0,
+            10.3609536,
+            10.5,
+            10.6390464,
+            11.0,
+            np.nan,
+            10.5,
+            10.6390464,
+            10.6390464,
+            np.nan,
+            np.nan,
+            np.nan,
+            11.0,
+            np.nan,
+            np.nan,
+        ]
+    )
+    res1 = np.array(
+        [
+            np.nan,
+            np.nan,
+            10.0,
+            np.nan,
+            np.nan,
+            np.nan,
+            10.0,
+            10.0,
+            10.0,
+            np.nan,
+            10.0,
+            10.0,
+            10.0,
+            11.0,
+            11.0,
+            np.nan,
+            10.0,
+            11.0,
+            11.0,
+            np.nan,
+            np.nan,
+            np.nan,
+            11.0,
+            np.nan,
+            np.nan,
+        ]
+    )
+    np.testing.assert_allclose(composite, res)
+    np.testing.assert_allclose(composite1, res1)
