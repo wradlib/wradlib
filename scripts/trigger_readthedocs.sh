40833126ab886298bd3e039f81177695f1bc2061
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2023-09-07 09:45:26 +0200

    MNT: update CI, remove/update bash-scripts

--- scripts/trigger_readthedocs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2011-2020, wradlib developers.
-# Distributed under the MIT License. See LICENSE.txt for more info.
-
-set -e
-
-if [ "$TRAVIS_PYTHON_VERSION" = "3.7" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
-    curl -X POST -d "token=$RTD_TOKEN" $RTD_URL
-fi
