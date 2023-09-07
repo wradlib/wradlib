48ea03c25fdfc921874386248ac7b57d5ee5d1cb
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2023-09-07 09:45:26 +0200

    MNT: update CI, remove/update bash-scripts

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,15 +1,6 @@
-#!/bin/bash
-# Copyright (c) 2011-2020, wradlib developers.
+#!/usr/bin/env bash
+# Copyright (c) 2011-2023, wradlib developers.
 # Distributed under the MIT License. See LICENSE.txt for more info.
 
 set -e
-
-if [[ "$TRAVIS" == "true" ]]; then
-    # export location of .coveragerc
-    export COVERAGE_PROCESS_START=$WRADLIB_BUILD_DIR/.coveragerc
-    coverage run -m pytest --verbose --doctest-modules --doctest-plus --durations=15 --pyargs $1
-    coverage combine
-    coverage xml
-else
-    pytest --verbose --doctest-modules --doctest-plus --durations=15 --pyargs $1
-fi
+pytest -n auto --dist loadfile --verbose --doctest-modules --doctest-plus --durations=15 --pyargs $1
