57659aa0fd60c556c87916d9b5c4b841a1d80aec
Kai Muehlbauer <kmuehlbauer@wradlib.org>
2026-01-12 11:29:28 +0100

    ENH: atten.py

--- wradlib/tests/conftest.py
+++ wradlib/tests/conftest.py
@@ -4,6 +4,10 @@
 
 import pytest
 
+from wradlib import georef, io, util
+
+wradlib_data = util.import_optional("wradlib_data", dep="devel")
+
 
 @pytest.fixture(params=["file", "filelike"])
 def file_or_filelike(request):
@@ -14,3 +18,10 @@ def file_or_filelike(request):
 def mock_wradlib_data_env(monkeypatch, tmpdir):
     wradlib_path = tmpdir.mkdir("wradlib-data")
     monkeypatch.setenv("WRADLIB_DATA", wradlib_path)
+
+
+@pytest.fixture(scope="session")
+def dx_swp():
+    fname = wradlib_data.DATASETS.fetch("dx/raa00-dx_10908-0806021655-fbg---bin.gz")
+    data, attrs = io.read_dx(fname)
+    return georef.create_xarray_dataarray(data)
