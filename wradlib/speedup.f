51ed328950a2cdf66c646ec4f871669d4f4f824c
enchilaDaSzen <eng.danielsr@outlook.com>
2026-05-29 14:19:14 +0200

    Improve unfold_phi: parameterise thresholds, fix naive implementation, update docstrings

--- wradlib/speedup.f
+++ wradlib/speedup.f
@@ -1,16 +1,17 @@
 ! Copyright (c) wradlib developers.
 ! Distributed under the MIT License. See LICENSE.txt for more info.
 C FILE: SPEEDUP.F
-      SUBROUTINE F_UNFOLD_PHI(PHIDP, RHO, GRADPHI, STDARR, BEAMS, RS, W)
+      SUBROUTINE F_UNFOLD_PHI(PHIDP,RHO,GPHI,STDARR,BEAMS,RS,W,TS,TR)
 C
 C     FORTRAN implementation of phase unfolding
 C
       INTEGER beams, rs, w
       REAL*4 phidp(beams,rs)
-      REAL*4 rho(beams,rs),gradphi(beams,rs), stdarr(beams,rs)
+      REAL*4 rho(beams,rs),gphi(beams,rs), stdarr(beams,rs)
+      REAL*4 ts, tr
       INTEGER beam, j, count1, count2, k, l, w_
       REAL*4 ref
-Cf2py intent(in) beams, rs, rho, gradphi, stdarr, w
+Cf2py intent(in) beams, rs, rho, gphi, stdarr, w, ts, tr
 Cf2py intent(in,out,overwrite) phidp
 
       w_ = w - 1
@@ -28,10 +29,10 @@ C        Determine location where meaningful phidpDP profile begins
             count1 = 0
             count2 = 0
             DO i=j, j+w_
-               IF (stdarr(beam,i)<5) THEN
+               IF (stdarr(beam,i)<ts) THEN
                   count1 = count1 + 1
                ENDIF
-               IF (rho(beam,i)>0.9) THEN
+               IF (rho(beam,i)>tr) THEN
                   count2 = count2 + 1
                ENDIF
             ENDDO
@@ -50,12 +51,12 @@ C       Now start to check for phase folding
             sumup = SUM( stdarr(beam,k-w_:k) )
             slopeok = 1
             DO l=k-w_, k
-               IF ((gradphi(beam,l)<-5) .OR. (gradphi(beam,l)>20)) THEN
+               IF ((gphi(beam,l)<-5) .OR. (gphi(beam,l)>20)) THEN
                   slopeok = 0
                ENDIF
             ENDDO
             IF ((slopeok==1) .AND. (sumup<15.)) THEN
-               ref = ref + gradphi(beam, k)*0.5
+               ref = ref + gphi(beam, k)*0.5
                IF (phidp(beam,k) - ref < -80.) THEN
                   IF (phidp(beam,k) < 0) THEN
                      phidp(beam,k) = phidp(beam,k) + 360.
